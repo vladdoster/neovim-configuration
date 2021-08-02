d34a06ce79ad7461f3088eb18bfb66dd1be40f9f
Vladislav Doster <mvdoster@gmail.com>
2021-08-02 06:53:28 -0500

    (maint): update options
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -1,55 +1,75 @@
-local function load_options()
-    local global_local = {
-        background = "dark",
-        backup = false, -- creates a backup file
-        clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-        cmdheight = 2, -- more space in the neovim command line for displaying messages
-        cmdwinheight = 5,
-        colorcolumn = "99999", -- fixes indentline for now
-        completeopt = {"menuone", "noselect"},
-        shortmess = {"s", "I"},
-        conceallevel = 0, -- so that `` is visible in markdown files
-        cursorline = true, -- highlight the current line
-        display = "lastline",
-        encoding = "utf-8",
-        expandtab = true, -- convert tabs to spaces
-        fileencoding = "utf-8", -- the encoding written to a file
-        foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-        foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
-        guifont = "JetBrains Mono Font:h15", -- the font used in graphical neovim applications
-        hidden = true, -- required to keep multiple buffers and open multiple buffers
-        hlsearch = true, -- highlight all matches on previous search pattern
-        ignorecase = true, -- ignore case in search patterns
-        mouse = "a", -- allow the mouse to be used in neovim
-        number = true, -- set numbered lines
-        numberwidth = 2, -- set number column width to 2 {default 4}
-        pumheight = 10, -- pop up menu height
-        relativenumber = true, -- set relative numbered lines
-        scrolloff = 8, -- is one of my fav
-        shiftwidth = 2, -- the number of spaces inserted for each indentation
-        showmode = false, -- we don't need to see things like -- INSERT -- anymore
-        showtabline = 2, -- always show tabs
-        sidescrolloff = 8,
-        signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-        smartcase = true, -- smart case
-        smartindent = true, -- make indenting smarter again
-        spell = false,
-        splitbelow = true, -- force all horizontal splits to go below current window
-        splitright = true, -- force all vertical splits to go to the right of current window
-        swapfile = false, -- creates a swapfile
-        tabstop = 4, -- insert 2 spaces for a tab
-        termguicolors = true, -- set term gui colors (most terminals support this)
-        timeoutlen = 100, -- time to wait for a mapped sequence to complete (in milliseconds)
-        title = true, -- set the title of window to the value of the titlestring
-        updatetime = 300, -- faster completion
-        wrap = false -- display lines as one long line
-    }
-    vim.g.python3_host_prog = '/usr/local/bin/python3'
-    for name, value in pairs(global_local) do vim.o[name] = value end
-end
+-- Neovim API aliases
+local cmd = vim.cmd     				-- execute Vim commands
+local exec = vim.api.nvim_exec 	-- execute Vimscript
+local fn = vim.fn       				-- call Vim functions
+local g = vim.g         				-- global variables
+local o = vim.o         				-- global options
+local b = vim.bo        				-- buffer-scoped options
+local w = vim.wo        				-- windows-scoped options
 
-load_options()
+-- General
+g.mapleader = ','           -- change leader to a comma
+o.mouse = 'a'               -- enable mous support
+o.background = "dark"
+o.guifont = "JetBrains Mono Font:h15" -- the font used in graphical neovim applications
+o.clipboard = 'unnamedplus' -- copy/paste to system clipboard
+b.swapfile = false          -- don't use swapfile
+vim.g.python3_host_prog = '/usr/local/bin/python3'
 
+-- Neovim UI
+o.syntax = 'enable'         -- enable syntax highlighting
+w.number = true             -- show line number
+o.showmatch = true          -- highlight matching parenthesis
+w.foldmethod = 'marker'     -- enable folding (default 'foldmarker')
+w.colorcolumn = '80'        -- line lenght marker at 80 columns
+o.splitright = true         -- vertical split to the right
+o.splitbelow = true         -- orizontal split to the bottom
+o.ignorecase = true         -- ignore case letters when search
+o.smartcase = true          -- ignore lowercase for the whole pattern
+
+-- Memory, CPU
+o.hidden = true         -- enable background buffers
+o.history = 100         -- remember n lines in history
+o.lazyredraw = true     -- faster scrolling
+b.synmaxcol = 240       -- max column for syntax highlight
+
+-- Colorscheme
+o.termguicolors = true          -- enable 24-bit RGB colors
+cmd([[colorscheme molokai]])    -- set colorscheme
+
+-- Tabs, indent
+b.expandtab = true      -- use spaces instead of tabs
+b.shiftwidth = 4        -- shift 4 spaces when tab
+b.tabstop = 4           -- 1 tab == 4 spaces
+b.smartindent = true    -- autoindent new lines
+
+-- don't auto commenting new lines
+cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+
+-- remove line lenght marker for selected filetypes
+cmd([[
+  autocmd FileType text,markdown,xml,html,xhtml,javascript setlocal cc=0
+]])
+
+-- 2 spaces for selected filetypes
+cmd([[
+  autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,go,python setlocal shiftwidth=4 tabstop=4
+]])
+
+-- IndentLine
+--g.indentLine_setColors = 0  -- set indentLine color
+-- g.indentLine_char = '|'       -- set indentLine character
+
+-- disable IndentLine for markdown files (avoid concealing)
+cmd([[
+  autocmd FileType markdown let g:indentLine_enabled=0
+]])
+
+-- Autocompletion
+o.completeopt = 'menuone,noselect,noinsert' -- completion options
+o.shortmess = 'c' 	-- don't show completion messagese
+
+-- Disable built-in plugins
 local disabled_built_ins = {
     "netrw", "netrwPlugin", "netrwSettings", "netrwFileHandlers", "gzip", "zip",
     "zipPlugin", "tar", "tarPlugin", "getscript", "getscriptPlugin", "vimball",
