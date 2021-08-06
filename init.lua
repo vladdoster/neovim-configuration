789eaad209e543d02e8db6b1d240ef9c3e76fa8f
vladislav doster <mvdoster@gmail.com>
2021-08-06 11:22:03 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
 local modules = {"plugins", "options", "mappings"}
 
-for i = 1, #modules, 1 do pcall(require, modules[i]) end
+for i = 1, #modules, 1 do pcall(
+    
+    
+    
+    require, modules[i]
+  
+  
+  )
+
+
+
+
+
+end
