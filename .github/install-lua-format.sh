246f7924880b0a5f44a361d125bd04e8641c5160
vladislav doster <mvdoster@gmail.com>
2021-08-06 18:30:05 -0500

    Update install-lua-format.sh

--- .github/install-lua-format.sh
+++ .github/install-lua-format.sh
@@ -1,7 +1,6 @@
 git clone --recurse-submodules https://github.com/Koihik/LuaFormatter.git /tmp/luaformatter
 pushd /tmp/luaformatter
-mkdir build 
-pushd build
-cmake .. -DBUILD_TESTS=FALSE 
-cmake --build . --target install 
-popd -2
+cmake .
+make
+make =j8 install
+popd -1
