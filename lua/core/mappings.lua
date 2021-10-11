303300594d2f078d98231a35ad9341dff09566b8
Vladislav Doster <mvdoster@gmail.com>
2021-10-10 20:29:41 -0500

    (maint): remove verbose map func, remove comments, and fix mappings
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,51 +1,59 @@
-local utils = require "core.utils"
-local map = utils.map
-local cmd = vim.cmd
 local M = {}
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+local cmd = vim.cmd
 
 M.misc = function()
-    map("v", "p", '"_dP')
-    map("", "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
-    map("", "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
-    map("", "<Down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
-    map("", "<Up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
-    map("n", "<Esc>", ":noh <CR>")
+    -- disable arrow keys
+    map('', '<up>', '<nop>', {noremap = true})
+    map('', '<down>', '<nop>', {noremap = true})
+    map('', '<left>', '<nop>', {noremap = true})
+    map('', '<right>', '<nop>', {noremap = true})
 
-    map({ "n", "v" }, "x", '"_x')
-    map({ "n", "v" }, "dd", '"_dd')
-    map("i", "<C-a>", "<ESC>^i")
+    map("n", "<Esc>", ":noh <CR>", opts)
+    map("v", "p", '"_dP', opts)
 
-    map("n", "<C-a>", ":%y+ <CR>") -- copy whole file content
-    map("n", "<S-t>", ":enew <CR>") -- new buffer
-    map("n", "<C-s>", ":w <CR>") -- ctrl + s to save file
-    map("v", "<C-s>", ":sort<CR>")
+    map("n", "x", '"_x', opts)
+    map("v", "x", '"_x', opts)
+    map("n", "dd", '"_dd', opts)
+    map("v", "dd", '"_dd', opts)
+    map("i", "<C-a>", "<ESC>^i", opts)
 
+    map("n", "<C-a>", ":%y+ <CR>", opts) -- copy whole file content
+    map("n", "<S-t>", ":enew <CR>", opts) -- new buffer
+    map("v", "<C-s>", ":sort<CR>", opts)
+    map('i', '<leader>s', '<C-c>:w<CR>', opts)
+    map('n', '<leader>s', ':w<CR>', opts)
     -- move lines up || down
-    map("v", "J", ":move '>+1<CR>gv-gv")
-    map("v", "K", ":move '<-2<CR>gv-gv")
-
+    map("v", "J", ":move '>+1<CR>gv-gv", opts)
+    map("v", "K", ":move '<-2<CR>gv-gv", opts)
     -- better indenting
-    map("v", ">", ">gv")
-    map("v", "<", "<gv")
+    map("v", ">", ">gv", opts)
+    map("v", "<", "<gv", opts)
+    -- move around splits using Ctrl + {h,j,k,l}
+    map('n', '<C-h>', '<C-w>h', opts)
+    map('n', '<C-j>', '<C-w>j', opts)
+    map('n', '<C-k>', '<C-w>k', opts)
+    map('n', '<C-l>', '<C-w>l', opts)
 
-    map("n", "<C-h>", "<C-w>h")
-    map("n", "<C-l>", "<C-w>l")
-    map("n", "<C-k>", "<C-w>k")
-    map("n", "<C-j>", "<C-w>j")
-    map("t", "<Esc>", "<C-\\><C-n><C-w>w")
+    map("t", "<Esc>", "<C-\\><C-n><C-w>w", opts)
 
-    map("n", "<A-Up>", ":resize -2<CR>")
-    map("n", "<A-Down>", ":resize +2<CR>")
-    map("n", "<A-Right>", ":vertical resize -2<CR>")
-    map("n", "<A-Left>", ":vertical resize +2<CR>")
+    map("n", "<A-Up>", ":resize -2<CR>", opts)
+    map("n", "<A-Down>", ":resize +2<CR>", opts)
+    map("n", "<A-Right>", ":vertical resize -2<CR>", opts)
+    map("n", "<A-Left>", ":vertical resize +2<CR>", opts)
 
-    map("n", "<Leader>bn", ":bufdo bnext<CR>")
-    map("n", "<Leader>bp", ":bufdo bprevious<CR>")
-    map("n", "<Leader>bc", ":bufdo bdelete<CR>")
+    map("n", "<Leader>bn", ":bufdo bnext<CR>", opts)
+    map("n", "<Leader>bp", ":bufdo bprevious<CR>", opts)
+    map("n", "<Leader>bc", ":bufdo bdelete<CR>", opts)
 
+    map('n', '<C-t>', ':Term<CR>', {noremap = true})
 
+    map('n', '<C-n>', ':NvimTreeToggle<CR>', opts) -- open/close
+    map('n', '<leader>r', ':NvimTreeRefresh<CR>', opts) -- refresh
+    map('n', '<leader>n', ':NvimTreeFindFile<CR>', opts) -- search file
 
-    map("n", "<leader>fm", ":Neoformat<CR>")
+    map("n", "<leader>fm", ":Neoformat<CR>", opts)
 
     cmd "silent! command PackerClean lua require 'plugins' require('packer').clean()"
     cmd "silent! command PackerCompile lua require 'plugins' require('packer').compile()"
@@ -54,20 +62,19 @@ M.misc = function()
     cmd "silent! command PackerSync lua require 'plugins' require('packer').sync()"
     cmd "silent! command PackerUpdate lua require 'plugins' require('packer').update()"
 
-    map("n", "<Leader>ps", ":PackerSync<CR>")
-    map("n", "<Leader>st", ":StartupTime<CR>")
-
+    map("n", "<Leader>ps", ":PackerSync<CR>", opts)
+    map("n", "<Leader>st", ":StartupTime<CR>", opts)
 end
 
 M.telescope = function()
-    map("n", "<leader>fb", ":Telescope buffers <CR>")
-    map("n", "<leader>ff", ":Telescope find_files <CR>")
-    map("n", "<leader>fa", ":Telescope find_files hidden=true <CR>")
-    map("n", "<leader>cm", ":Telescope git_commits <CR>")
-    map("n", "<leader>gt", ":Telescope git_status <CR>")
-    map("n", "<leader>fh", ":Telescope help_tags <CR>")
-    map("n", "<leader>fw", ":Telescope live_grep <CR>")
-    map("n", "<leader>fo", ":Telescope oldfiles <CR>")
+    map("n", "<leader>fb", ":Telescope buffers <CR>", opts)
+    map("n", "<leader>ff", ":Telescope find_files <CR>", opts)
+    map("n", "<leader>fa", ":Telescope find_files hidden=true <CR>", opts)
+    map("n", "<leader>cm", ":Telescope git_commits <CR>", opts)
+    map("n", "<leader>gt", ":Telescope git_status <CR>", opts)
+    map("n", "<leader>fh", ":Telescope help_tags <CR>", opts)
+    map("n", "<leader>fw", ":Telescope live_grep <CR>", opts)
+    map("n", "<leader>fo", ":Telescope oldfiles <CR>", opts)
 end
 
 return M
--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,6 +4,7 @@ if not present then
 end
 
 vim.g.nvim_tree_ignore = { ".git", "node_modules", "dist" }
+vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_show_icons = {
     git = 1,
@@ -33,14 +34,6 @@ vim.g.nvim_tree_icons = {
         open = "",
     },
 }
-vim.api.nvim_set_keymap("n", "<C-n>", ":NvimTreeToggle<CR>", {
-    noremap = true,
-    silent = true,
-})
-vim.api.nvim_set_keymap("n", "<leader>r", ":NvimTreeRefresh<CR>", {
-    noremap = true,
-    silent = true,
-})
 
 require("nvim-tree").setup {
     disable_netrw = true,
@@ -77,56 +70,3 @@ require("nvim-tree").setup {
     },
 }
 
--- require("nvim-tree").setup {
---     disable_netrw = true,
---     hijack_netrw = true,
---     open_on_setup = true,
---     ignore_ft_on_setup = {},
---     auto_close = true,
---     open_on_tab = false,
---     update_to_buf_dir = true,
---     hijack_cursor = false,
---     update_cwd = true,
---     lsp_diagnostics = false,
---     update_focused_file = {
---         enable = false,
---         update_cwd = true,
---         ignore_list = {
---             ".git",
---             "node_modules",
---             ".cache",
---             "build",
---             "__pycache__",
---         },
---     },
---     system_open = {
---         cmd = nil,
---         args = {},
---     },
---     view = {
---         width = 30,
---         side = "left",
---         auto_resize = true,
---         mappings = {
---             custom_only = false,
---             list = {},
---         },
---     },
--- }
---
--- g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
--- g.nvim_tree_gitignore = 1
--- g.nvim_tree_quit_on_open = 1
--- g.nvim_tree_indent_markers = 1
--- g.nvim_tree_hide_dotfiles = 1
--- g.nvim_tree_git_hl = 1
--- g.nvim_tree_highlight_opened_files = 1
--- g.nvim_tree_root_folder_modifier = ":~"
--- g.nvim_tree_add_trailing = 1
--- g.nvim_tree_group_empty = 1
--- g.nvim_tree_disable_window_picker = 1
--- g.nvim_tree_icon_padding = " "
--- g.nvim_tree_symlink_arrow = " >> "
--- g.nvim_tree_respect_buf_cwd = 0
--- g.nvim_tree_create_in_closed_folder = 0
--- g.nvim_tree_refresh_wait = 500
