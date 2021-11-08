97547aa784c098b3f5fb7217ab0513565a168df8
Vladislav Doster <Vladislav.doster@Mastercard.com>
2021-11-08 07:22:06 -0600

    (bugfix): call setup() for surround plugin
    Signed-off-by: Vladislav Doster <Vladislav.doster@Mastercard.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,71 +6,60 @@ return packer.startup {
     function()
         use {'wbthomason/packer.nvim'}
         use {'lewis6991/impatient.nvim'}
-        use {'dstein64/vim-startuptime', cmd='StartupTime'}
+        use {'dstein64/vim-startuptime', cmd = 'StartupTime'}
         -- UI
         use {'kyazdani42/nvim-web-devicons'}
-        use {'marko-cerovac/material.nvim', config=[[require 'plugins.others'.material()]]}
-        use {'famiu/feline.nvim', config=[[require 'plugins.feline']]}
-        use {'akinsho/nvim-bufferline.lua', config=[[require 'plugins.bufferline']]}
+        use {'marko-cerovac/material.nvim', config = [[require 'plugins.others'.material()]]}
+        use {'famiu/feline.nvim', config = [[require 'plugins.feline']]}
+        use {'akinsho/nvim-bufferline.lua', config = [[require 'plugins.bufferline']]}
         -- GENERAL
-        use {'andymass/vim-matchup', config=[[require 'plugins.matchup']]}
-        use {'kyazdani42/nvim-tree.lua', config=[[require 'plugins.nvimtree']]}
-        use {'lewis6991/gitsigns.nvim', config=[[require 'plugins.gitsigns']]}
-        use {'lukas-reineke/format.nvim', config=[[require 'plugins.format']]}
-        use {'s1n7ax/nvim-terminal', config=[[require 'nvim-terminal'.setup()]]}
-        use {'winston0410/commented.nvim', config=[[require 'commented'.setup()]]}
-        use {'blackCauldron7/surround.nvim'}
+        use {'andymass/vim-matchup', config = [[require 'plugins.matchup']]}
+        use {'kyazdani42/nvim-tree.lua', config = [[require 'plugins.nvimtree']]}
+        use {'lewis6991/gitsigns.nvim', config = [[require 'plugins.gitsigns']]}
+        use {'lukas-reineke/format.nvim', config = [[require 'plugins.format']]}
+        use {'s1n7ax/nvim-terminal', config = [[require 'nvim-terminal'.setup()]]}
+        use {'winston0410/commented.nvim', config = [[require 'commented'.setup()]]}
+        use {"blackCauldron7/surround.nvim", config = [[require"surround".setup {mappings_style = "surround"} ]]}
         use {'junegunn/vim-easy-align'}
         use {'tpope/vim-repeat'}
         -- LSP
         use {'williamboman/nvim-lsp-installer'}
-        use {'tami5/lspsaga.nvim', config=[[require 'plugins.lsp-saga']]}
-        use {'neovim/nvim-lspconfig', config=[[require 'plugins.lsp-config']]}
+        use {'tami5/lspsaga.nvim', config = [[require 'plugins.lsp-saga']]}
+        use {'neovim/nvim-lspconfig', config = [[require 'plugins.lsp-config']]}
         use {
             'hrsh7th/nvim-cmp',
-            config=[[require 'plugins.cmp']],
-            requires={
-                {'L3MON4D3/LuaSnip', config=[[require 'plugins.others'.luasnip()]]},
-                {'lukas-reineke/cmp-under-comparator'},
-                {'f3fora/cmp-spell'},
-                {'hrsh7th/cmp-buffer'},
-                {'hrsh7th/cmp-nvim-lsp'},
-                {'hrsh7th/cmp-nvim-lua'},
-                {'hrsh7th/cmp-path'},
-                {'hrsh7th/cmp-vsnip'},
-                {'onsails/lspkind-nvim'},
-                {'rafamadriz/friendly-snippets'},
-                {'ray-x/cmp-treesitter'},
+            config = [[require 'plugins.cmp']],
+            requires = {
+                {'L3MON4D3/LuaSnip', config = [[require 'plugins.others'.luasnip()]]},
+                {'lukas-reineke/cmp-under-comparator'}, {'f3fora/cmp-spell'}, {'hrsh7th/cmp-buffer'},
+                {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-nvim-lua'}, {'hrsh7th/cmp-path'}, {'hrsh7th/cmp-vsnip'},
+                {'onsails/lspkind-nvim'}, {'rafamadriz/friendly-snippets'}, {'ray-x/cmp-treesitter'},
                 {'saadparwaiz1/cmp_luasnip'}
             }
         }
-        use {
-            'windwp/nvim-autopairs',
-            after={'nvim-cmp'},
-            config=[[require 'plugins.others'.autopairs()]]
-        }
-        use {'nvim-telescope/telescope-fzf-native.nvim', run='make'}
+        use {'windwp/nvim-autopairs', after = {'nvim-cmp'}, config = [[require 'plugins.others'.autopairs()]]}
+        use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make'}
         use {
             'nvim-telescope/telescope.nvim', -- fuzzy finder
-            cmd='Telescope',
-            config=[[require 'plugins.telescope']],
-            module='telescope',
-            requires={{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
-            setup=[[require 'core.mappings'.telescope()]]
+            cmd = 'Telescope',
+            config = [[require 'plugins.telescope']],
+            module = 'telescope',
+            requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+            setup = [[require 'core.mappings'.telescope()]]
         }
         use {
             'nvim-treesitter/nvim-treesitter', -- syntax highlighting
-            config=[[require 'plugins.treesitter']],
-            event='BufRead',
-            run=':TSUpdate',
-            requires={
-                {'nvim-treesitter/nvim-treesitter-refactor', opt=true},
-                {'nvim-treesitter/nvim-treesitter-textobjects', opt=true}
+            config = [[require 'plugins.treesitter']],
+            event = 'BufRead',
+            run = ':TSUpdate',
+            requires = {
+                {'nvim-treesitter/nvim-treesitter-refactor', opt = true},
+                {'nvim-treesitter/nvim-treesitter-textobjects', opt = true}
             }
         }
     end,
-    config={
-        compile_path=vim.fn.stdpath('config') .. '/lua/packer_compiled.lua',
-        profile={enable=true, threshold=1}
+    config = {
+        compile_path = vim.fn.stdpath('config') .. '/lua/packer_compiled.lua',
+        profile = {enable = true, threshold = 1}
     }
 }
