251f5e770d5d48de5ecfe33e6d62c918b3bb1227
Vladislav Doster <Vladislav.doster@Mastercard.com>
2021-12-08 02:45:14 +0000

    (maint) mappings for easy term buffer navigation
    Signed-off-by: Vladislav Doster <Vladislav.doster@Mastercard.com>

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -17,3 +17,16 @@ require("toggleterm").setup{
   close_on_exit   = true,        -- close   the terminal window when the        process exits
   shell           = vim.o.shell, -- change  the default  shell
 }
+
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true}
+  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
