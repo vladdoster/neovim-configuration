24014a3dbc999771016164818a6b02e901b6d6ed
Vladislav Doster <mvdoster@gmail.com>
2021-12-14 00:28:15 -0600

    (maint) update loading bug
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,6 @@
 local utils = require 'core.utils'
 local map = utils.map
 local cmd = vim.cmd
---local M = {}
---
---
 local M = {}
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', {noremap=true, silent=true})
 vim.g.mapleader = ' '
@@ -12,7 +9,6 @@ vim.g.maplocalleader = ' '
 vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 --Remap for dealing with word wrap
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
