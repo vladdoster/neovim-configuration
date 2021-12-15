56f7130c9f9533581740e497d83cc25b187972d5
Vladislav Doster <mvdoster@gmail.com>
2021-12-15 01:36:35 -0600

    (bugfix): loading packer_compiled & feline status
    - load `packer_compiled` last in `core/init.lua`
    - comment out `feline` statusline diagnostics due to
      it using deprecated methods
    - remove `terraform` with `hcl` treesitter syntax
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,7 @@
 local ok, impatient = pcall(require, 'impatient')
 if ok then impatient.enable_profile() end
-local ok, packer = pcall(require, 'packer_compiled')
-for _, module in ipairs({'options', 'autocmds', 'mappings'}) do
+for _, module in ipairs({'options', 'autocmds', 'mappings',}) do
     local ok, err = pcall(require, 'core.' .. module)
     if not ok then error('Error loading ' .. module .. '\n\n' .. err) end
 end
+pcall(require, 'packer_compiled')
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
 if status_ok then
   local opts = {
-    ensure_installed = {'python', 'go', 'terraform'},
+    ensure_installed = {'python', 'go', 'hcl', 'markdown', 'make', 'dockerfile'},
     ignore_install = {},
     highlight = { enable = true, additional_vim_regex_highlighting = true, disable = { "latex" } },
     context_commentstring = { enable = false, config = { json = "", },
