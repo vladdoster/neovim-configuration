99e1db8b05d30e210791c8e93c633cbb68251a1c
Vladislav Doster <mvdoster@gmail.com>
2022-01-22 20:59:02 -0600

    (maint): update shfmt fmt config
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -2,20 +2,20 @@ local present, format = pcall(require, 'format')
 if present then
     format.setup {
         ['*']={{cmd={'sed -i \'s/[ \t]*$//\''}}},
-        bash={{cmd={'shfmt -i=2 -s -ln=bash -sr -bn -ci -w'}}},
+        bash={{cmd={'shfmt -bn -ci -i 2 -sr -w'}}},
         go={{cmd={'gofmt -w', 'goimports -w'}, tempfile_postfix='.tmp'}},
         lua={{cmd={'lua-format --column-limit 120 --in-place'}}},
         python={{cmd={'black -l 160', 'isort'}, tempfile_postfix='.tmp'}},
         sh={{cmd={'shfmt -i 2 -s -sr -bn -ci -w'}}},
         terraform={{cmd={'terraform fmt'}}},
         yaml={{cmd={'yamlfmt -w'}}},
-        zsh={{cmd={'shfmt -i 4 -s -ln bash -sr -bn -ci -w'}}},
+        zsh={{cmd={'shfmt -i 2 -s -ln bash -sr -bn -ci -w'}}},
         markdown={
             {cmd={'mdformat --wrap 100'}},
             {cmd={'black'}, start_pattern='^```python$', end_pattern='^```$', target='current'},
             {
                 cmd={'shfmt -bn -ci -i 2 -sr -s -w'},
-                start_pattern='^```bash$',
+                start_pattern='^```(bash|sh|shell)$',
                 end_pattern='^```$',
                 target='current'
             },
