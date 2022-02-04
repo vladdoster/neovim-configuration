e7efd618d2e38af63489b009ee1e4d769606ded3
Vladislav Doster <mvdoster@gmail.com>
2022-02-03 20:02:31 -0600

    (maint) update cmp, lsp-config, and misc. options
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,55 +1,25 @@
 local present, cmp = pcall(require, 'cmp')
 if not present then return end
 vim.opt.completeopt = 'menuone,noselect'
-cmp.setup {
-    snippet={expand=function(args) require('luasnip').lsp_expand(args.body) end},
-    formatting={
-        format=function(entry, vim_item)
-            vim_item.menu = ({
-                rg='rg',
-                nvim_lsp='LSP',
-                nvim_lua='Lua',
-                Path='Path',
-                luasnip='LuaSnip',
-                treesitter='ts'
-            })[entry.source.name]
-            vim_item.kind = ({
-                Text='',
-                Method='',
-                Function='',
-                Constructor='',
-                Field='ﰠ',
-                Variable='',
-                Class='ﴯ',
-                Interface='',
-                Module='',
-                Property='ﰠ',
-                Unit='塞',
-                Value='',
-                Enum='',
-                Keyword='',
-                Snippet='',
-                Color='',
-                File='',
-                Reference='',
-                Folder='',
-                EnumMember='',
-                Constant='',
-                Struct='פּ',
-                Event='',
-                Operator='',
-                TypeParameter=''
-            })[vim_item.kind]
-            return vim_item
-        end
-    },
-    mapping={
-        ['<C-p>']=cmp.mapping.select_prev_item(),
-        ['<C-n>']=cmp.mapping.select_next_item(),
-        ['<C-d>']=cmp.mapping.scroll_docs(-4),
-        ['<C-f>']=cmp.mapping.scroll_docs(4),
-        ['<C-Space>']=cmp.mapping.complete(),
-        ['<C-e>']=cmp.mapping.close(),
+
+  cmp.setup({
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+      },
+      mapping = {
+        ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+          i = cmp.mapping.abort(),
+          c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<CR>']=cmp.mapping.confirm {
             behavior=cmp.ConfirmBehavior.Replace,
             select=true
@@ -76,14 +46,25 @@ cmp.setup {
                 fallback()
             end
         end
-    },
-    sources={
-        {name='buffer'},
-        {name='luasnip'},
-        {name='nvim_lsp'},
-        {name='nvim_lua'},
-        {name='path'},
-        {name='rg'},
-        {name='treesitter'}
-    }
-}
+      },
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      })
+    })
+    -- `/` cmdline setup.
+    cmp.setup.cmdline('/', {
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(':', {
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      })
+    })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -35,7 +35,7 @@ local function on_attach(_, bufnr)
     map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
     map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.documentationFormat = {
     'markdown',
     'plaintext'
