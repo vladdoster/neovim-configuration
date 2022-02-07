44e477241c7e1695da3ba446fd214615ad14ee45
Vladislav Doster <mvdoster@gmail.com>
2022-02-07 11:39:28 -0600

    style: format auto commands in utils.lua
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,13 +1,13 @@
 local cmd = vim.cmd
-vim.cmd[[au BufEnter * set fo-=c fo-=r fo-=o]]
-vim.cmd[[au BufEnter *.py set ai sw=4 ts=4 sta et fo=croql]]
-vim.cmd[[au BufWinEnter * checktime]]
-vim.cmd[[au BufWritePre * :%s/\s\+$//e]]
-vim.cmd[[au InsertEnter * set norelativenumber]]
-vim.cmd[[au InsertLeave * set relativenumber]]
-vim.cmd[[au TextYankPost * silent! lua vim.highlight.on_yank()]]
-vim.cmd[[au VimResized * tabdo wincmd =]]
-vim.cmd[[autocmd BufReadPost *
+vim.cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
+vim.cmd [[au BufEnter *.py set ai sw=4 ts=4 sta et fo=croql]]
+vim.cmd [[au BufWinEnter * checktime]]
+vim.cmd [[au BufWritePre * :%s/\s\+$//e]]
+vim.cmd [[au InsertEnter * set norelativenumber]]
+vim.cmd [[au InsertLeave * set relativenumber]]
+vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank()]]
+vim.cmd [[au VimResized * tabdo wincmd =]]
+vim.cmd [[autocmd BufReadPost *
         \ if line("'\"") > 1
         \ && line("'\"") <= line("$")
         \ | execute "normal! g`\""
