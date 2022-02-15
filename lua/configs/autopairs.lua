ff447847f5aef875f2abcc562e6d5ea11bc21476
Vladislav Doster <mvdoster@gmail.com>
2022-02-15 13:14:18 +0000

    fix: add `end` to autopairs config function
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/autopairs.lua
+++ lua/configs/autopairs.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 function M.config()
-	local loaded_0, autopairs = pcall(require, "nvim-autopairs")
-	local loaded_1, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
-	local loaded_2, cmp = pcall(require, "cmp")
-	if loaded_0 and loaded_1 and loaded_2 then
-		autopairs.setup(default)
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-	end
+    local loaded_0, autopairs = pcall(require, 'nvim-autopairs')
+    local loaded_1, cmp_autopairs = pcall(require,
+                                          'nvim-autopairs.completion.cmp')
+    local loaded_2, cmp = pcall(require, 'cmp')
+    if loaded_0 and loaded_1 and loaded_2 then
+        autopairs.setup(default)
+        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end
+end
 return M
