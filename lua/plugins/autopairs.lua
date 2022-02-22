38733a1ccc63be18aaccd45da75326cf69a1cde9
Vladislav Doster <mvdoster@gmail.com>
2022-02-22 02:58:57 -0600

    fix: autopairs annoying behavior removed
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,12 +1,26 @@
 local M = {}
 
 function M.config()
-  local loaded_0, autopairs = pcall(require, 'nvim-autopairs')
-  local loaded_1, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
-  local loaded_2, cmp = pcall(require, 'cmp')
-  if loaded_0 and loaded_1 and loaded_2 then
-    autopairs.setup(default)
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  local ok_0, autopairs = pcall(require, 'nvim-autopairs')
+  local ok_1, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
+  local ok_2, cmp = pcall(require, 'cmp')
+  if (ok_0 and ok_1 and ok_2) then
+    require('nvim-autopairs').setup({
+        enable_check_bracket_line = true,
+        ignored_next_char = "[%w%.]", 
+        fast_wrap = {
+          map = '<M-e>',
+          chars = { '{', '[', '(', '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+          offset = 0, -- Offset from pattern match
+          end_key = '$',
+          keys = 'qwertyuiopzxcvbnmasdfghjkl',
+          check_comma = true,
+          highlight = 'Search',
+          highlight_grey='Comment'
+        },
+    })
+      cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
   end
 end
 return M
