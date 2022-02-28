d0a92b778f663e187fbd08273a70b32e8d0747f6
Vladislav Doster <mvdoster@gmail.com>
2022-02-28 00:12:10 -0600

    refactor: remove unused lsp configs & exand cmp/lsp capabilities
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ lua/plugins/lsp/cmp.lua
@@ -0,0 +1,48 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	completion = {
+		keyword_length = 2,
+	},
+	mapping = {
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end,
+	},
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "path" },
+		{ name = "buffer" },
+	},
+})
--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,25 +1,22 @@
-local nls = require('null-ls')
-local U = require('plugins.lsp.utils')
-local fmt = nls.builtins.formatting
-local dgn = nls.builtins.diagnostics
+local nls, U = require("null-ls"), require("plugins.lsp.utils")
+local fmt, dgn = nls.builtins.formatting, nls.builtins.diagnostics
 nls.setup({
-  sources = {
-    fmt.trim_whitespace.with({
-      filetypes = {'text', 'sh', 'zsh', 'toml', 'make', 'conf', 'tmux'}
-    }),
-    fmt.prettierd,
-    fmt.eslint_d,
-    fmt.rustfmt,
-    fmt.stylua,
-    fmt.terraform_fmt,
-    fmt.gofmt,
-    fmt.shfmt,
-    dgn.eslint_d,
-    dgn.shellcheck,
-    dgn.luacheck.with({extra_args = {'--globals', 'vim', '--std', 'luajit'}})
-  },
-  on_attach = function(client, bufnr)
-    U.fmt_on_save(client)
-    U.mappings(bufnr)
-  end
+	sources = {
+		fmt.trim_whitespace.with({
+			filetypes = { "text", "sh", "zsh", "toml", "make", "conf", "tmux" },
+		}),
+		fmt.prettierd,
+		fmt.eslint_d,
+		fmt.stylua,
+		fmt.terraform_fmt,
+		fmt.gofmt,
+		fmt.shfmt,
+		dgn.eslint_d,
+		dgn.shellcheck,
+		dgn.luacheck.with({ extra_args = { "--globals", "vim", "--std", "luajit" } }),
+	},
+	on_attach = function(client, bufnr)
+		U.fmt_on_save(client)
+		U.mappings(bufnr)
+	end,
 })
