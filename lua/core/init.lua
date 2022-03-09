22d796d03cd75dc7edb6f5d5580d2d31792b0ebd
Vladislav Doster <Vladislav.Doster@mastercard.com>
2022-03-08 18:07:09 -0600

    chore: test misc. changes
    Signed-off-by: Vladislav Doster <Vladislav.Doster@mastercard.com>

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,3 +1,3 @@
-require('core.options')
-require('core.mappings')
-require('core.autocmds')
+require("core.options")
+require("core.autocmds")
+require("core.mappings")
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,62 +1,72 @@
+vim.g.did_load_filetypes = 0 -- Disable vim-based filetype plugin
+vim.g.do_filetype_lua = 1 -- Enable lua-based filetype plugin
+vim.filetype.add({
+	extension = { zsh = "sh" },
+	pattern = { [".*%.env.*"] = "sh", [".*ignore"] = "conf" },
+})
+
 local g = vim.g
 local disabled_built_ins = {
-  '2html_plugin',
-  'getscript',
-  'getscriptPlugin',
-  'gzip',
-  'logipat',
-  'netrw',
-  'netrwPlugin',
-  'netrwSettings',
-  'netrwFileHandlers',
-  'matchit',
-  'tar',
-  'tarPlugin',
-  'rrhelper',
-  'spellfile_plugin',
-  'vimball',
-  'vimballPlugin',
-  'zip',
-  'zipPlugin'
+	"2html_plugin",
+	"getscript",
+	"getscriptPlugin",
+	"gzip",
+	"logipat",
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"matchit",
+	"tar",
+	"tarPlugin",
+	"rrhelper",
+	"spellfile_plugin",
+	"vimball",
+	"vimballPlugin",
+	"zip",
+	"zipPlugin",
 }
-for _, plugin in pairs(disabled_built_ins) do g['loaded_' .. plugin] = 1 end
+for _, plugin in pairs(disabled_built_ins) do
+	g["loaded_" .. plugin] = 1
+end
 local options = {
-  autoindent = true,
-  background = 'dark',
-  backup = false, -- creates a backup file
-  clipboard = 'unnamedplus', -- allows neovim to access the system clipboard
-  cmdheight = 2, -- more space in the neovim command line for displaying messages
-  completeopt = {'menuone', 'noselect'}, -- mostly just for cmp
-  conceallevel = 0, -- so that `` is visible in markdown files
-  cursorline = true, -- highlight the current line
-  expandtab = true, -- convert tabs to spaces
-  fileencoding = 'utf-8', -- the encoding written to a file
-  -- guifont = 'monospace:h17', -- the font used in graphical neovim applications
-  -- hlsearch = true, -- highlight all matches on previous search pattern
-  ignorecase = true, -- ignore case in search patterns
-  mouse = 'a', -- allow the mouse to be used in neovim
-  number = true, -- set numbered lines
-  numberwidth = 4, -- set number column width to 2 {default 4}
-  pumheight = 10, -- pop up menu height
-  scrolloff = 8, -- is one of my fav
-  shiftwidth = 2, -- the number of spaces inserted for each indentation
-  showmode = false, -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2, -- always show tabs
-  sidescrolloff = 8,
-  signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
-  smartcase = true, -- smart case
-  smartindent = true, -- make indenting smarter again
-  splitbelow = true, -- force all horizontal splits to go below current window
-  splitright = true, -- force all vertical splits to go to the right of current window
-  swapfile = false, -- creates a swapfile
-  tabstop = 2, -- insert 2 spaces for a tab
-  termguicolors = true, -- set term gui colors (most terminals support this)
-  undofile = true, -- enable persistent undo
-  updatetime = 300, -- faster completion (4000ms default)
-  wrap = false, -- display lines as one long line
-  writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+	background = "dark",
+	backup = false,
+	cindent = true,
+	clipboard = "unnamedplus",
+	cursorline = true,
+	expandtab = true,
+	history = 50,
+	ignorecase = true,
+	number = true,
+	numberwidth = 6,
+	autoindent = true,
+	backupdir = "/tmp/",
+	directory = "/tmp/",
+	formatoptions = "qrn1",
+	smarttab = true,
+	undodir = "/tmp/",
+	relativenumber = true,
+	scrolloff = 8,
+	shiftwidth = 0,
+	signcolumn = "yes",
+	smartcase = true,
+	softtabstop = -1,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	tabstop = 4,
+	termguicolors = true,
+	textwidth = 300,
+	timeoutlen = 500,
+	undofile = true,
+	updatetime = 200,
+	wrap = true,
+	writebackup = false,
 }
 -- vim.opt.shortmess:append("c")
-for k, v in pairs(options) do vim.opt[k] = v end
-vim.cmd('set whichwrap+=<,>,[,],h,l')
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+vim.cmd("set whichwrap+=<,>,[,],h,l")
 vim.cmd([[set iskeyword+=-]])
