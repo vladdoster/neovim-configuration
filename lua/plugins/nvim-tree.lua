8e9d776bdddd5e6c3da2c92b53c361ff745f5dbb
Vladislav Doster <mvdoster@gmail.com>
2022-03-21 03:53:59 -0500

    maint(nvim-tree): minor QoL config updates
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,30 +3,36 @@ if not status_ok then return end
 
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
+g.nvim_tree_add_trailing = 0
+g.nvim_tree_git_hl = 0
+g.nvim_tree_highlight_opened_files = 0
 g.nvim_tree_icons = {
   default='',
   symlink='->',
-  git={unstaged='~', staged='+', unmerged='!', renamed='≈', untracked='?', deleted='-'}
+  git={deleted='-', renamed='≈', staged='+', unmerged='!', unstaged='~', untracked='?'}
 }
+g.nvim_tree_indent_markers = 1
+g.nvim_tree_root_folder_modifier = table.concat{':t:gs?$?/..', string.rep(' ', 1000), '?:gs?^??'}
+
 nvim_tree.setup{
-  disable_netrw=true,
-  hijack_netrw=true,
-  open_on_setup=false,
+  actions={open_file={resize_window=true}},
   auto_close=true,
-  open_on_tab=false,
-  hijack_cursor=false,
-  update_cwd=true,
-  update_to_buf_dir={enable=true, auto_open=true},
   diagnostics={enable=false},
-  update_focused_file={enable=true, update_cwd=true, ignore_list={}},
-  system_open={cmd=nil, args={}},
+  disable_netrw=true,
+  disable_window_picker=0,
   filters={dotfiles=false, custom={}},
   git={enable=true, ignore=true, timeout=500},
-  view={width=30, height=30, hide_root_folder=false, side='right', auto_resize=true, number=true},
-  quit_on_open=0,
   git_hl=1,
-  disable_window_picker=0,
-  root_folder_modifier=':t',
-  show_icons={files=0, folder_arrows=1, folders=1, git=0, tree_width=30}
+  hijack_cursor=true,
+  hijack_netrw=true,
+  hijack_unnamed_buffer_when_opening=false,
+  open_on_setup=false,
+  open_on_tab=false,
+  quit_on_open=0,
+  show_icons={files=0, folder_arrows=1, folders=1, git=0, tree_width=30},
+  system_open={cmd=nil, args={}},
+  update_cwd=true,
+  update_focused_file={enable=true, update_cwd=true, ignore_list={}},
+  update_to_buf_dir={enable=true, auto_open=true},
+  view={width=30, height=30, hide_root_folder=false, side='right', auto_resize=true, number=true}
 }
