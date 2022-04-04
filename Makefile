8bcb9cda1167e1a8044c99ce1221d9fb29f5c14e
Vladislav Doster <mvdoster@gmail.com>
2022-04-04 00:05:34 -0500

    feat: switch to stylua formatter
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- Makefile
+++ Makefile
@@ -2,11 +2,10 @@ all: fmt
 deps:
 	luarocks install --server=https://luarocks.org/dev luaformatter
 fmt:
-	@find . -name '*.lua' -print -exec \
-		lua-format \
-		--config .lua_format.yml \
-		--in-place \
-		-- {} \;
+	@find . \
+		-name '*.lua' \
+		-print \
+		-exec stylua -f /Users/anonymous/.config/nvim/.stylua.toml  {} \;
 clean:
-	find . -name 'packer_compiled*' -type f -print -delete
-	find $$HOME/.local/share/ -maxdepth 1 -name 'nvim' -type d -print -exec rm -rf {} \;
+	@find . -name 'packer_compiled*' -type f -print -delete
+	@find $$HOME/.local/share/ -maxdepth 1 -name 'nvim' -type d -print -exec rm -rf {} \;
--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -1,34 +1,25 @@
-local nls = require('null-ls')
-local U = require('configs.lsp.utils')
+local nls = require 'null-ls'
+local U = require 'configs.lsp.utils'
 
 local fmt = nls.builtins.formatting
 local dgn = nls.builtins.diagnostics
 
 -- Configuring null-ls
-nls.setup({
+nls.setup {
   sources = { -- # FORMATTING #
-    dgn.luacheck.with({
+    dgn.luacheck.with {
       extra_args = { '--globals', 'vim', '--std', 'luajit' },
       method = nls.methods.DIAGNOSTICS_ON_SAVE,
-    }),
+    },
     dgn.hadolint,
-    dgn.pylint.with({ method = nls.methods.DIAGNOSTICS_ON_SAVE }),
+    dgn.pylint.with { method = nls.methods.DIAGNOSTICS_ON_SAVE },
     dgn.shellcheck,
     dgn.zsh,
     fmt.gofmt,
-    fmt.shfmt.with({ extra_args = { '-i', 2, '-s', '-ci', '-sr', '-bn' } }),
-    fmt.stylua.with({
-      extra_args = {
-        '--quote-style',
-        'ForceSingle',
-        '--indent-type',
-        'Spaces',
-        '--indent-width',
-        2,
-        '--column-width',
-        120,
-      },
-    }),
+    fmt.shfmt.with { extra_args = { '-i', 2, '-s', '-ci', '-sr', '-bn' } },
+    fmt.stylua.with {
+      extra_args = { '-f', '$HOME/.config/nvim/.stylua.toml' },
+    },
     fmt.terraform_fmt,
     fmt.trim_newlines,
     fmt.trim_whitespace,
@@ -37,4 +28,4 @@ nls.setup({
     U.fmt_on_save(client)
     U.mappings(bufnr)
   end,
-})
+}
