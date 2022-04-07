f08ac1d3a62852e3e890a047f31cfc7eba41f4f4
Vladislav Doster <mvdoster@gmail.com>
2022-04-07 10:38:59 -0500

    update: flesh out treesitter configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,17 +1,23 @@
-local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
-if not status_ok then
+local M = {}
+local loaded, treesitter = pcall(require, 'nvim-treesitter.configs')
+if not loaded then
   return
 end
-configs.setup {
-  ensure_installed = { 'bash', 'hcl', 'java', 'python', 'yaml' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { '' }, -- List of parsers to ignore installing
+treesitter.setup {
   autopairs = { enable = true },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { '' }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { 'yaml' } },
+  incremental_selection = { enable = true },
+  autotag = { enable = true },
   context_commentstring = { enable = true, enable_autocmd = false },
+  ensure_installed = { 'bash', 'hcl', 'java', 'python', 'yaml' }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  ignore_install = {},
+  indent = { enable = false },
+  rainbow = {
+    enable = true,
+    disable = { 'html' },
+    extended_mode = false,
+    max_file_lines = nil,
+  },
+  sync_install = false,
 }
+return M
