bd2f70882adc1b91e4a906f201d22cb971fe1465
Vladislav Doster <mvdoster@gmail.com>
2022-05-10 15:50:16 +0000

    maint: remove unused treesitter config code
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -71,16 +71,3 @@ ts_configs.setup {
     },
   },
 }
-
--- Add Markdown
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
--- parser_config.jsonc.used_by = "json"
-local ft_to_parser = require('nvim-treesitter.parsers').filetype_to_parsername
-ft_to_parser.json = 'jsonc' -- the someft filetype will use the python parser and queries.
-
-parser_config.wgsl = {
-  install_info = {
-    url = 'https://github.com/szebniok/tree-sitter-wgsl',
-    files = { 'src/parser.c' },
-  },
-}
