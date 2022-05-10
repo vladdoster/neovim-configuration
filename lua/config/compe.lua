d2b50624664657c8ac2d175b9358b70960c74227
Vladislav Doster <mvdoster@gmail.com>
2022-05-10 06:23:25 -0500

    maint: update nvim-cmp configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/compe.lua
+++ lua/config/compe.lua
@@ -5,9 +5,25 @@ vim.o.completeopt = 'menuone,noselect'
 -- Setup nvim-cmp.
 local cmp = require 'cmp'
 
+local wincfg = { winhighlight = 'Normal:CmpNormal,FloatBorder:CmpBorder,CursorLine:CmpSel,Search:None' }
 cmp.setup {
   completion = {
-    completeopt = 'menu,menuone,noinsert',
+    completeopt = 'menu,menuone,noselect',
+    get_trigger_characters = function(trigger_characters)
+      return vim.tbl_filter(function(char)
+        return char ~= ' '
+      end, trigger_characters)
+    end,
+  },
+  window = {
+    completion = wincfg,
+    documentation = wincfg,
+  },
+  view = {
+    entries = { name = 'custom', selection_order = 'bottom_up' },
+  },
+  experimental = {
+    ghost_text = true,
   },
   snippet = {
     expand = function(args)
@@ -22,22 +38,28 @@ cmp.setup {
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm { select = true },
   },
-  sources = {
-    { name = 'nvim_lsp' },
+  sources = cmp.config.sources({
+    { name = 'buffer', keyword_length = 5 },
     { name = 'luasnip' },
-    { name = 'buffer' },
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
     { name = 'path' },
-  },
+    { name = 'treesitter' },
+  }, { { name = 'buffer' } }),
   formatting = {
-    format = require('config.lsp.kind').cmp_format(),
-  },
-  -- documentation = {
-  --   border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  --   winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
-  -- },
-  experimental = {
-    ghost_text = {
-      hl_group = 'LspCodeLens',
+    format = require('config.lsp.kind').cmp_format {
+      before = function(entry, vim_item)
+        vim_item.menu = ({
+          buffer = 'Buf',
+          git = 'Git',
+          luasnip = 'Snip',
+          nvim_lsp = 'LSP',
+          nvim_lua = 'Lua',
+          path = 'Path',
+          treesitter = 'TS',
+        })[entry.source.name]
+        return vim_item
+      end,
     },
   },
   sorting = {
