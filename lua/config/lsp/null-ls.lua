ab119f2d77edcd67cfffcee626f267b317450a44
Vladislav Doster <mvdoster@gmail.com>
2022-05-12 08:58:52 +0000

    style: add checkmake and fix trim whitespace null-ls sources
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/lsp/null-ls.lua
+++ lua/config/lsp/null-ls.lua
@@ -1,27 +1,27 @@
 local M = {}
 
 function M.setup(options)
-  local nls = require("null-ls")
-  nls.setup({
+  local nls = require 'null-ls'
+  nls.setup {
     debounce = 150,
     save_after_format = false,
     sources = {
-      nls.builtins.formatting.prettierd,
-      nls.builtins.formatting.stylua,
-      nls.builtins.formatting.fixjson.with({ filetypes = { "jsonc" } }),
-      nls.builtins.diagnostics.shellcheck,
-      nls.builtins.diagnostics.markdownlint,
+      nls.builtins.diagnostics.checkmake,
       nls.builtins.diagnostics.selene,
-      nls.builtins.formatting.trimwhitespace,
+      nls.builtins.diagnostics.shellcheck,
+      nls.builtins.diagnostics.zsh,
+      nls.builtins.formatting.fixjson.with { filetypes = { 'jsonc' } },
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.trim_whitespace,
     },
     on_attach = options.on_attach,
-    root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".nvim.settings.json", ".git"),
-  })
+    root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.nvim.settings.json', '.git'),
+  }
 end
 
 function M.has_formatter(ft)
-  local sources = require("null-ls.sources")
-  local available = sources.get_available(ft, "NULL_LS_FORMATTING")
+  local sources = require 'null-ls.sources'
+  local available = sources.get_available(ft, 'NULL_LS_FORMATTING')
   return #available > 0
 end
 
