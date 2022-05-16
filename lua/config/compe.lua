f0921c5bb8d0cf39d8ed19e32082c87116a2afee
Vladislav Doster <mvdoster@gmail.com>
2022-05-16 00:39:49 -0500

    fix: nvim-cmp mappings use tab now
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/compe.lua
+++ lua/config/compe.lua
@@ -1,6 +1,3 @@
-vim.o.completeopt = 'menuone,noselect'
-
--- Setup nvim-cmp.
 local cmp = require 'cmp'
 
 local wincfg = { winhighlight = 'Normal:CmpNormal,FloatBorder:CmpBorder,CursorLine:CmpSel,Search:None' }
@@ -29,49 +26,49 @@ cmp.setup {
     end,
   },
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+      elseif require('luasnip').expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
       else
         fallback()
       end
     end, {
-      "i",
-      "s",
+      'i',
+      's',
     }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+      elseif require('luasnip').jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
       else
         fallback()
       end
     end, {
-      "i",
-      "s",
+      'i',
+      's',
     }),
   },
-  sources = cmp.config.sources({
+  sources = cmp.config.sources {
     { name = 'buffer', keyword_length = 2 },
     { name = 'luasnip' },
     { name = 'nvim_lsp' },
     { name = 'nvim_lua' },
     { name = 'path' },
     { name = 'treesitter' },
-  }, { { name = 'buffer' } }),
+  },
   formatting = {
     format = require('config.lsp.kind').cmp_format {
       before = function(entry, vim_item)
@@ -88,7 +85,14 @@ cmp.setup {
       end,
     },
   },
-  sorting = { comparators = { cmp.config.compare.offset, cmp.config.compare.order, cmp.config.compare.score, cmp.config.compare.sort_text } },
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.order,
+      cmp.config.compare.score,
+      cmp.config.compare.sort_text,
+    },
+  },
 }
 local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done { map_char = { tex = '' } })
