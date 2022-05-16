66f1156250407b63bd8d36a2a3d9e7f4aaa5425b
Vladislav Doster <mvdoster@gmail.com>
2022-05-16 23:36:58 +0000

    fix: remove majority of treeitter config until bug breaking syntax highlighting is found
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,73 +1,15 @@
-local ts_configs = require 'nvim-treesitter.configs'
-ts_configs.setup {
-  ensure_installed = { 'bash', 'go', 'lua', 'markdown', 'python', 'toml', 'yaml' },
-  highlight = { enable = true, use_languagetree = true },
-  indent = { enable = false },
-  context_commentstring = { enable = true, enable_autocmd = false },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '<C-n>',
-      node_incremental = '<C-n>',
-      scope_incremental = '<C-s>',
-      node_decremental = '<C-r>',
-    },
+local present, treesitter = pcall(require, 'nvim-treesitter.configs')
+if not present then
+  return
+end
+local options = {
+  ensure_installed = {
+    'lua',
+    'vim',
   },
-  query_linter = {
+  highlight = {
     enable = true,
-    use_virtual_text = true,
-    lint_events = { 'BufWrite', 'CursorHold' },
-  },
-  textsubjects = {
-    enable = true,
-    keymaps = {
-      ['.'] = 'textsubjects-smart',
-      [';'] = 'textsubjects-container-outer',
-    },
-  },
-  playground = {
-    enable = true,
-    disable = {},
-    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries = true, -- Whether the query persists across vim sessions
-    keybindings = {
-      toggle_query_editor = 'o',
-      toggle_hl_groups = 'i',
-      toggle_injected_languages = 't',
-      toggle_anonymous_nodes = 'a',
-      toggle_language_display = 'I',
-      focus_language = 'f',
-      unfocus_language = 'F',
-      update = 'R',
-      goto_node = '<cr>',
-      show_help = '?',
-    },
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = { [']m'] = '@function.outer', [']]'] = '@class.outer' },
-      goto_next_end = { [']M'] = '@function.outer', [']['] = '@class.outer' },
-      goto_previous_start = { ['[m'] = '@function.outer', ['[['] = '@class.outer' },
-      goto_previous_end = { ['[M'] = '@function.outer', ['[]'] = '@class.outer' },
-    },
-    lsp_interop = {
-      enable = true,
-      peek_definition_code = {
-        ['gD'] = '@function.outer',
-      },
-    },
+    use_languagetree = true,
   },
 }
+treesitter.setup(options)
