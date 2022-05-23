c1bd3867a5ab32beaeb668d04cf15aada01be6f9
Vladislav Doster <mvdoster@gmail.com>
2022-05-23 06:28:10 -0500

    maint: increase nvim formatter timeout
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -3,20 +3,22 @@ local M = {}
 function M.setup(options)
   local nls = require 'null-ls'
   local format, diagnostics = nls.builtins.formatting, nls.builtins.diagnostics
+  vim.lsp.buf.format { timeout_ms = 2000 } -- nvim v0.8.0
+  -- vim.lsp.buf.formatting_sync(nil, 2000) -- v0.7.0
   nls.setup {
-    debounce=150,
-    save_after_format=false,
-    sources={
+    debounce = 150,
+    save_after_format = false,
+    sources = {
       diagnostics.markdownlint,
       diagnostics.shellcheck,
       format.black,
       format.reorder_python_imports,
       format.stylua,
       format.terraform_fmt,
-      format.trim_whitespace
+      format.trim_whitespace,
     },
-    on_attach=options.on_attach,
-    root_dir=require('null-ls.utils').root_pattern('.null-ls-root', '.nvim.settings.json', '.git')
+    on_attach = options.on_attach,
+    root_dir = require('null-ls.utils').root_pattern('.null-ls-root', '.nvim.settings.json', '.git'),
   }
 end
 
