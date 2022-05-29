2b219b017d23c0802634d5055b0e69aab517acc9
Vladislav Doster <mvdoster@gmail.com>
2022-05-29 03:17:38 -0500

    maint: update notify.nvim size and show duration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/notify.lua
+++ lua/configs/notify.lua
@@ -2,16 +2,13 @@ local M = {}
 local notify_ok, notify = pcall(require, 'notify')
 if not notify_ok then return end
 notify.setup {
-  background_colour='Normal',
   icons={DEBUG='debug:', ERROR='err:', INFO='info:', TRACE='trace:', WARN='warn:'},
   max_height=nil,
-  max_width=nil,
-  minimum_width=50,
-  on_close=nil,
-  on_open=nil,
+  max_width=50,
+  minimum_width=30,
   render='default',
   stages='fade',
-  timeout=5000
+  timeout=3000
 }
 vim.notify = notify
 return M
