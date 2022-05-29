30a7682ef7d7d17ebdc710cb1d72dd1f5521649b
Vladislav Doster <mvdoster@gmail.com>
2022-05-29 03:26:06 -0500

    style: format null-ls, telescope, and lsp-installer configs
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/lsp/lsp-installer.lua
+++ lua/configs/lsp/lsp-installer.lua
@@ -1,11 +1,10 @@
 local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
 if not status_ok then return end
 
-require('nvim-lsp-installer').setup({
-  ensure_installed={'pyright', 'dockerls', 'bashls', 'terraformls', 'gopls', 'pylsp'},
+require('nvim-lsp-installer').setup {
+  ensure_installed={'pyright', 'dockerls', 'bashls', 'terraform', 'go'},
   automatic_installation=true,
   ui={icons={server_installed='+', server_pending='~', server_uninstalled='-'}},
   pip={install_args={}},
-  log_level=vim.log.levels.INFO,
   max_concurrent_installers=4
-})
+}
