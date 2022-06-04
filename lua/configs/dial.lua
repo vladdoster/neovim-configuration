1a14cd1d32cc7dcc2c06dc045321feb4d4c9e324
Vladislav Doster <mvdoster@gmail.com>
2022-06-04 08:04:18 -0500

    feat: add dial.nvim to add {in,de}rement features
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ lua/configs/dial.lua
@@ -0,0 +1,5 @@
+local status_ok, augend = pcall(require, 'dial.augend')
+if not status_ok then return end
+require('dial.config').augends:register_group{
+  default={augend.constant.alias.bool, augend.integer.alias.decimal}
+}
