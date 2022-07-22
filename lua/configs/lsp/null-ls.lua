60d8da66fd054c07b5d4e199f3983f4b38267f0a
Vladislav Doster <mvdoster@gmail.com>
2022-07-22 17:25:47 -0500

    fix: use v0.8.0 api for null-ls formatting
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/lsp/null-ls.lua
+++ lua/configs/lsp/null-ls.lua
@@ -2,19 +2,26 @@ local nls, U = require('null-ls'), require('configs.lsp.utils')
 local dgn, fmt = nls.builtins.diagnostics, nls.builtins.formatting
 
 local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-local lsp_fmt = function(bufnr)
+
+local lsp_formatting = function(bufnr)
+  -- vim.lsp.buf.formatting_sync({ filter = function(client) return client.name == "null-ls" end, bufnr = bufnr, })
   vim.lsp.buf.format({filter=function(client) return client.name == 'null-ls' end, bufnr=bufnr})
 end
 
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+
+-- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-  U.mappings(bufnr)
   if client.supports_method('textDocument/formatting') then
     vim.api.nvim_clear_autocmds({group=augroup, buffer=bufnr})
-    vim.api.nvim_create_autocmd('BufWritePre', {group=augroup, buffer=bufnr, callback=function() lsp_fmt(bufnr) end})
+    vim.api.nvim_create_autocmd('BufWritePre',
+                                {group=augroup, buffer=bufnr, callback=function() lsp_formatting(bufnr) end})
   end
 end
-
-nls.setup({
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+require('null-ls').setup({
+  -- you can reuse a shared lspconfig on_attach callback here
   debounce=150,
   debug=false,
   save_after_format=false,
@@ -32,5 +39,19 @@ nls.setup({
     fmt.terraform_fmt,
     fmt.trim_whitespace
   },
-  on_attach=on_attach
+  on_attach=function(client, bufnr)
+    if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_clear_autocmds({group=augroup, buffer=bufnr})
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group=augroup,
+        buffer=bufnr,
+        callback=function()
+          -- nvim 0.8 -> vim.lsp.buf.format({ bufnr = bufnr })
+          -- nvim 0.7 -> vim.lsp.buf.formatting_sync()
+          -- vim.lsp.buf.formatting_sync()
+          vim.lsp.buf.format({buf=bufnr})
+        end
+      })
+    end
+  end
 })
