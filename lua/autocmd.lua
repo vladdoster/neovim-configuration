2db55bfa8349ec0934dd677aa486f1fd39a9a578
Vladislav Doster <mvdoster@gmail.com>
2022-07-25 13:55:00 +0000

    maint: update autocmds to new Lua API
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,37 +1,9 @@
-vim.g.did_load_filetypes = 0 -- Disable vim-based filetype plugin
-vim.g.do_filetype_lua = 1 -- Enable lua-based filetype plugin
-vim.cmd [[
-  augroup _global_settings
-    autocmd!
-
-    autocmd BufEnter * set fo-=c fo-=r fo-=o
-    autocmd BufWinEnter * :set formatoptions-=cro
-
-    autocmd FileType help,startuptime,qf,lspinfo nnoremap <buffer><silent> q :close<CR>
-    autocmd FileType man nnoremap <buffer><silent> q :quit<CR>
-
-    autocmd FileType qf set nobuflisted
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-
-    autocmd FocusGained * :checktime
-
-    autocmd InsertEnter * set norelativenumber
-    autocmd InsertEnter,WinLeave * set nocursorline
-
-    autocmd InsertLeave * set relativenumber
-    autocmd InsertLeave,WinEnter * set cursorline
-
-    autocmd TextYankPost * lua vim.highlight.on_yank {}
-  augroup end
-
-  augroup _git_and_md_settings
-    autocmd!
-    autocmd FileType gitcommit,markdown setlocal wrap
-    autocmd FileType gitcommit,markdown setlocal spell
-  augroup end
-
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
-]]
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('BufEnter', { pattern = '*', command = 'set fo-=c fo-=r fo-=o' })
+autocmd('FileType', { pattern = 'gitcommit,markdown', command = 'setlocal spell' })
+autocmd('FileType', { pattern = 'gitcommit,markdown', command = 'setlocal wrap' })
+autocmd('FileType', { pattern = 'help,lspinfo,man,qf,startuptime', command = 'nnoremap <buffer><silent> q :close<CR>' })
+autocmd('FileType', { pattern = 'qf', callback = function() vim.opt_local.buflisted = false end })
+autocmd('InsertEnter', { pattern = '*', command = 'set relativenumber' })
+autocmd('InsertLeave,WinEnter', { pattern = '*', command = 'set cursorline' })
+autocmd('TextYankPost', { callback = function() vim.highlight.on_yank({ higroup = 'Visual', timeout = 120 }) end })
