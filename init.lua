33d3c7bd3efd1c7ce400b18d5399b4643d00fd58
Vladislav Doster <mvdoster@gmail.com>
2022-07-26 23:50:20 -0500

    maint: add error handling to loading config
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
-local present, impatient = pcall(require, 'impatient')
-if present then impatient.enable_profile() end
-require 'settings'
-require 'plugins'
-require 'keybinds'
-require 'autocmd'
+local status_ok, impatient = pcall(require, 'impatient')
+if status_ok then impatient.enable_profile() end
+
+for _, source in ipairs {'settings', 'plugins', 'keybinds', 'autocmd'} do
+  local found, fault = pcall(require, source)
+  if not found then vim.api.nvim_err_writeln('Failed to load ' .. source .. '\n\n' .. fault) end
+end
