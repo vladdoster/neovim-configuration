43eb6e03f6870c201eb573e94150f7a88eed11d1
Vladislav Doster <Vladislav.Doster@Mastercard.com>
2022-08-04 09:17:04 -0500

    (maint): fix cmp configuration
    Signed-off-by: Vladislav Doster <Vladislav.Doster@Mastercard.com>

--- lua/configs/lsp/cmp.lua
+++ lua/configs/lsp/cmp.lua
@@ -1,45 +1,62 @@
 local status_ok, cmp = pcall(require, 'cmp')
 if not status_ok then return end
-local cmp = require('cmp')
+
+vim.opt.completeopt = 'menuone,noselect'
+
+local function border(hl_name)
+  return {
+    {'╭', hl_name},
+    {'─', hl_name},
+    {'╮', hl_name},
+    {'│', hl_name},
+    {'╯', hl_name},
+    {'─', hl_name},
+    {'╰', hl_name},
+    {'│', hl_name}
+  }
+end
+
+local cmp_window = require 'cmp.utils.window'
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function(self)
+  local info = self:info_()
+  info.scrollable = false
+  return info
+end
 
 cmp.setup({
-  behavior=cmp.SelectBehavior.Insert,
-  completion={keyword_length=1},
-  confirm_opts={behavior=cmp.ConfirmBehavior.Replace, select=true},
-  window={documentation=false},
-  duplicates={nvim_lsp=1, luasnip=1, buffer=1, path=1},
-  experimental={ghost_text=false},
-  view={entries='native'},
+  window={
+    completion={border=border 'CmpBorder', winhighlight='Normal:CmpPmenu,CursorLine:PmenuSel,Search:None'},
+    documentation={border=border 'CmpDocBorder'}
+  },
   snippet={expand=function(args) require('luasnip').lsp_expand(args.body) end},
   mapping={
-    ['<C-p>']=cmp.mapping(cmp.mapping.select_prev_item({behavior=cmp.SelectBehavior.Insert}), {'i', 'c'}),
-    ['<C-d>']=cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-    ['<C-f>']=cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-    ['<C-Space>']=cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-    ['<C-e>']=cmp.mapping(cmp.mapping.confirm({behavior=cmp.ConfirmBehavior.Replace, select=true}), {'i', 'c'}),
-    ['<CR>']=cmp.mapping.confirm({behavior=cmp.ConfirmBehavior.Replace, select=false}),
-    ['<Tab>']=cmp.mapping(cmp.mapping.select_next_item({behavior=cmp.SelectBehavior.Insert}), {'i', 'c'}),
-    ['<S-Tab>']=cmp.mapping(cmp.mapping.select_prev_item({behavior=cmp.SelectBehavior.Insert}), {'i', 'c'})
+    ['<C-p>']=cmp.mapping.select_prev_item(),
+    ['<C-n>']=cmp.mapping.select_next_item(),
+    ['<C-d>']=cmp.mapping.scroll_docs(-4),
+    ['<C-f>']=cmp.mapping.scroll_docs(4),
+    ['<C-Space>']=cmp.mapping.complete(),
+    ['<C-e>']=cmp.mapping.close(),
+    ['<CR>']=cmp.mapping.confirm {behavior=cmp.ConfirmBehavior.Replace, select=false},
+    ['<Tab>']=cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require('luasnip').expand_or_jumpable() then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+    ['<S-Tab>']=cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require('luasnip').jumpable(-1) then
+        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+      else
+        fallback()
+      end
+    end, {'i', 's'})
   },
-  sources=cmp.config.sources({
-    {name='nvim_lsp', max_item_count=10},
-    {name='buffer', max_item_count=10},
-    {name='luasnip', max_item_count=10},
-    {name='path', max_item_count=10}
-  })
-})
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources=cmp.config.sources({
-    {name='cmp_git'} -- You can specify the `cmp_git` source if you were installed it.
-  }, {{name='buffer'}})
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {mapping=cmp.mapping.preset.cmdline(), sources={{name='buffer'}}})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping=cmp.mapping.preset.cmdline(),
-  sources=cmp.config.sources({{name='path'}}, {{name='cmdline'}})
+  sources={{name='luasnip'}, {name='nvim_lsp'}, {name='buffer'}, {name='nvim_lua'}, {name='path'}, {name='cmdline'}}
 })
