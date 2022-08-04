ce49097de1fc6d7ac9f53cc4d3a703fd52173efa
Vladislav Doster <Vladislav.Doster@Mastercard.com>
2022-08-04 09:16:30 -0500

    (maint): add mason plugin config
    Signed-off-by: Vladislav Doster <Vladislav.Doster@Mastercard.com>

---
+++ lua/configs/mason.lua
@@ -0,0 +1,13 @@
+local M = {}
+local mason_ok, mason = pcall(require, 'mason')
+if not mason_ok then return end
+
+mason.setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
