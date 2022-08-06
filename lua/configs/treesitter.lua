38d5748ba7198bb27afbe4adc9e7031d225b20d3
Vladislav Doster <mvdoster@gmail.com>
2022-08-06 08:01:07 -0500

    maint: remove cruft in treesitter configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -3,16 +3,10 @@ if vim.api.nvim_list_uis() == 0 then return end
 local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then return end
 treesitter.setup {
-  ensure_installed={'bash', 'go', 'hcl', 'java', 'json', 'python', 'python', 'yaml'},
+  ensure_installed={'bash', 'dockerfile', 'go', 'hcl', 'json', 'python', 'python', 'yaml'},
   ignore_install={'haskell', 'phpdoc'},
   highlight={enable=true},
   indent={enable=true, disable={'python', 'yaml'}},
-  playground={
-    enable=true,
-    disable={},
-    updatetime=25, -- Debounced time for highlighting nodes in the playground from source code
-    persist_queries=false -- Whether the query persists across vim sessions
-  },
   incremental_selection={
     enable=true,
     keymaps={
@@ -21,10 +15,7 @@ treesitter.setup {
       node_decremental='<S-Tab>' -- visual mode
     }
   },
-  -- extensions
-  textobjects={select={enable=true, keymaps={['af']='@function.outer', ['if']='@function.inner'}}},
-  autotag={enable=true, filetypes={'html', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue'}},
+  autotag={enable=true, filetypes={'markdown'}},
   rainbow={enable=true},
-  context_commentstring={enable=true, config={javascriptreact={style_element='{/*%s*/}'}}},
   matchup={enable=true}
 }
