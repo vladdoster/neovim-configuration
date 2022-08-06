e121b0129706514f5465387b36267be1591b59cb
Vladislav Doster <mvdoster@gmail.com>
2022-08-06 06:31:54 -0500

    (maint): update mason configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -1,13 +1,23 @@
-local M = {}
 local mason_ok, mason = pcall(require, 'mason')
 if not mason_ok then return end
 
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+  ui={icons={package_installed='+', package_pending='~', package_uninstalled='-'}}
+})
+
+local mason_lspconfig_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
+if not mason_lspconfig_ok then return end
+
+mason_lspconfig.setup({
+  ensure_installed={
+    'bash-language-server',
+    'black',
+    'dockerfile-language-server',
+    'hadolint',
+    'luacheck',
+    'luaformatter',
+    'pyright',
+    'sumneko_lua'
+  },
+  automatic_installation=true
 })
