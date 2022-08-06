4a39e31a76ad4faf6a911e363032bcfeb5ea9452
Vladislav Doster <mvdoster@gmail.com>
2022-08-06 06:31:20 -0500

    (maint): update lsp configurations
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/lsp/cmp.lua
+++ lua/configs/lsp/cmp.lua
@@ -1,19 +1,14 @@
-local status_ok, cmp = pcall(require, 'nvim-cmp')
-if not status_ok then return end
+local cmp_ok, cmp = pcall(require, 'cmp')
+if not cmp_ok then return end
+
+local types = require 'cmp.types'
+local luasnip = require 'luasnip'
+local mapping = cmp.mapping
 
 vim.opt.completeopt = 'menuone,noselect'
 
-local function border(hl_name)
-  return {
-    {'╭', hl_name},
-    {'─', hl_name},
-    {'╮', hl_name},
-    {'│', hl_name},
-    {'╯', hl_name},
-    {'─', hl_name},
-    {'╰', hl_name},
-    {'│', hl_name}
-  }
+local function border(hl)
+  return {{'╭', hl}, {'─', hl}, {'╮', hl}, {'│', hl}, {'╯', hl}, {'─', hl}, {'╰', hl}, {'│', hl}}
 end
 
 local cmp_window = require 'cmp.utils.window'
@@ -25,38 +20,61 @@ cmp_window.info = function(self)
   return info
 end
 
-cmp.setup({
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
+end
+
+cmp.setup {
   window={
     completion={border=border 'CmpBorder', winhighlight='Normal:CmpPmenu,CursorLine:PmenuSel,Search:None'},
     documentation={border=border 'CmpDocBorder'}
   },
-  snippet={expand=function(args) require('luasnip').lsp_expand(args.body) end},
+  formatting={format=require('lspkind').cmp_format {with_text=true}},
+  snippet={expand=function(args) luasnip.lsp_expand(args.body) end},
   mapping={
-    ['<C-p>']=cmp.mapping.select_prev_item(),
-    ['<C-n>']=cmp.mapping.select_next_item(),
-    ['<C-d>']=cmp.mapping.scroll_docs(-4),
-    ['<C-f>']=cmp.mapping.scroll_docs(4),
-    ['<C-Space>']=cmp.mapping.complete(),
-    ['<C-e>']=cmp.mapping.close(),
-    ['<CR>']=cmp.mapping.confirm {behavior=cmp.ConfirmBehavior.Replace, select=false},
-    ['<Tab>']=cmp.mapping(function(fallback)
+    ['<C-d>']=mapping(mapping.scroll_docs(8), {'i'}),
+    ['<C-u>']=mapping(mapping.scroll_docs(-8), {'i'}),
+    ['<C-k>']=mapping(function(fallback)
+      if cmp.open_docs_preview() then
+        cmp.close()
+      else
+        fallback()
+      end
+    end),
+    ['<C-Space>']=mapping.complete(),
+    ['<C-e>']=mapping.abort(),
+    ['<CR>']=mapping.confirm {select=false},
+    ['<C-n>']=mapping.select_next_item {behavior=types.cmp.SelectBehavior.Select},
+    ['<C-p>']=mapping.select_prev_item {behavior=types.cmp.SelectBehavior.Select},
+    ['<Tab>']=mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
-      elseif require('luasnip').expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+        cmp.select_next_item {behavior=types.cmp.SelectBehavior.Select}
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
     end, {'i', 's'}),
-    ['<S-Tab>']=cmp.mapping(function(fallback)
+    ['<S-Tab>']=mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require('luasnip').jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+        cmp.select_prev_item {behavior=types.cmp.SelectBehavior.Select}
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
     end, {'i', 's'})
   },
-  sources={{name='luasnip'}, {name='nvim_lsp'}, {name='buffer'}, {name='nvim_lua'}, {name='path'}, {name='cmdline'}}
-})
+  sources={
+    {name='nvim_lsp'},
+    {name='buffer'},
+    {name='path'},
+    {name='luasnip'},
+    {name='git'},
+  }
+}
+
+require('cmp_git').setup {enableRemoteUrlRewrites=true}
--- lua/configs/lsp/servers.lua
+++ lua/configs/lsp/servers.lua
@@ -3,44 +3,32 @@ vim.diagnostic.config({virtual_text={source='always'}, float={source='always'}})
 -- Lua
 -- Use default configuration
 
-local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
-if not status_ok then return end
-local lsp, U = require('lspconfig'), require('configs.lsp.utils')
-local default_servers = {'bashls', 'dockerls', 'gopls', 'jsonls', 'pylsp', 'pyright', 'terraformls', 'yamlls'}
-local lsp_installer = require('nvim-lsp-installer')
-local lspconfig = require('lspconfig')
+-- local lsp, U = require('lspconfig'), require('configs.lsp.utils')
+-- local default_servers = {'bashls', 'dockerls', 'gopls', 'jsonls', 'pylsp', 'pyright', 'terraformls', 'yamlls'}
+-- local lspconfig = require('lspconfig')
 
--- 1. Set up nvim-lsp-installer first!
-lsp_installer.setup {
-  automatic_installation=true,
-  ensure_installed=default_servers,
-  max_concurrent_installers=10,
-  pip={install_args={}},
-  ui={icons={server_installed='+', server_pending='~', server_uninstalled='-'}}
-}
-
-lspconfig.util.default_config = vim.tbl_extend('force', lspconfig.util.default_config, {
-  capabilities=require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-  flags={allow_incremental_sync=true, debounce_text_changes=200},
-  on_attach=require'aerial'.on_attach
-})
+-- lspconfig.util.default_config = vim.tbl_extend('force', lspconfig.util.default_config, {
+--   capabilities=require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+--   flags={allow_incremental_sync=true, debounce_text_changes=200},
+--   on_attach=require'aerial'.on_attach
+-- })
 
 -- 3. Loop through all of the installed servers and set it up via lspconfig
-for _, server in ipairs(lsp_installer.get_installed_servers()) do
-  if server.name ~= 'sumneko_lua' then
-    lsp.sumneko_lua.setup({
-      on_attach=require('aerial').on_attach,
-      settings={
-        Lua={
-          completion={enable=true, showWord='Disable'},
-          runtime={version='LuaJIT'},
-          diagnostics={globals={'vim', 'dump'}},
-          workspace={library={os.getenv('VIMRUNTIME')}},
-          telemetry={enable=false}
-        }
-      }
-    })
-  else
-    lspconfig[server.name].setup {}
-  end
-end
+-- for _, server in ipairs(lsp_installer.get_installed_servers()) do
+--   if server.name ~= 'sumneko_lua' then
+--     lsp.sumneko_lua.setup({
+--       on_attach=require('aerial').on_attach,
+--       settings={
+--         Lua={
+--           completion={enable=true, showWord='Disable'},
+--           runtime={version='LuaJIT'},
+--           diagnostics={globals={'vim', 'dump'}},
+--           workspace={library={os.getenv('VIMRUNTIME')}},
+--           telemetry={enable=false}
+--         }
+--       }
+--     })
+--   else
+--     lspconfig[server.name].setup {}
+--   end
+-- end
