caca4157da6ab4487038cb3912b1205c47cd251f
Vladislav Doster <mvdoster@gmail.com>
2022-08-06 06:32:42 -0500

    (maint): update treesitter configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,21 +1,30 @@
-local M = {}
--- avoid running in headless mode since it's harder to detect failures
-if #vim.api.nvim_list_uis() == 0 then return end
+if vim.api.nvim_list_uis() == 0 then return end
 
 local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then return end
-
 treesitter.setup {
-  autopairs={enable=true},
-  autotag={enable=true},
-  context_commentstring={enable=true, enable_autocmd=false},
   ensure_installed={'bash', 'go', 'hcl', 'java', 'json', 'python', 'python', 'yaml'},
-  highlight={enable=true, use_languagetree=true},
-  ignore_install={},
-  incremental_selection={enable=true},
-  indent={enable=false},
-  rainbow={disable={'html'}, enable=true, extended_mode=false, max_file_lines=nil},
-  sync_install=false
+  ignore_install={'haskell', 'phpdoc'},
+  highlight={enable=true},
+  indent={enable=true, disable={'python', 'yaml'}},
+  playground={
+    enable=true,
+    disable={},
+    updatetime=25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries=false -- Whether the query persists across vim sessions
+  },
+  incremental_selection={
+    enable=true,
+    keymaps={
+      init_selection='<S-Tab>', -- normal mode
+      node_incremental='<Tab>', -- visual mode
+      node_decremental='<S-Tab>' -- visual mode
+    }
+  },
+  -- extensions
+  textobjects={select={enable=true, keymaps={['af']='@function.outer', ['if']='@function.inner'}}},
+  autotag={enable=true, filetypes={'html', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue'}},
+  rainbow={enable=true},
+  context_commentstring={enable=true, config={javascriptreact={style_element='{/*%s*/}'}}},
+  matchup={enable=true}
 }
-
-return M
