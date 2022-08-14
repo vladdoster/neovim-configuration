7342d4e66ef5bed00a60120c2e16ff227fbc85f0
Vladislav Doster <mvdoster@gmail.com>
2022-08-14 08:33:18 -0500

    fix: simplify treesitter configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,21 +1,10 @@
-if vim.api.nvim_list_uis() == 0 then return end
+local treesitter_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
+if not treesitter_ok then return end
 
-local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
-if not status_ok then return end
 treesitter.setup {
-  ensure_installed={'bash', 'dockerfile', 'go', 'hcl', 'json', 'python', 'python', 'yaml'},
-  ignore_install={'haskell', 'phpdoc'},
-  highlight={enable=true},
-  indent={enable=true, disable={'python', 'yaml'}},
-  incremental_selection={
-    enable=true,
-    keymaps={
-      init_selection='<S-Tab>', -- normal mode
-      node_incremental='<Tab>', -- visual mode
-      node_decremental='<S-Tab>' -- visual mode
-    }
-  },
-  autotag={enable=true, filetypes={'markdown'}},
-  rainbow={enable=true},
-  matchup={enable=true}
+  ensure_installed={'bash', 'dockerfile', 'go', 'hcl', 'json', 'python', 'toml', 'yaml'},
+  highlight={enable=true, use_languagetree=true},
+  indent={enable=true},
+  matchup={enable=true},
+  rainbow={enable=true}
 }
