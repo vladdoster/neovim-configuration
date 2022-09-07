6ac24f65c77db65c09b1819cb9a2a575899716e3
Vladislav Doster <mvdoster@gmail.com>
2022-09-06 23:50:17 -0500

    feat: add MasonInstallAll cmd installs desired lsp
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/Mason.lua
+++ plugin/Mason.lua
@@ -4,3 +4,7 @@ mason.setup {
   max_concurrent_installers=10,
   ui={icons={package_installed='', package_pending='', package_uninstalled=''}}
 }
+
+local lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
+local usr_cmd = vim.api.nvim_create_user_command
+usr_cmd('MasonInstallAll', function() vim.cmd('MasonInstall ' .. table.concat(lsp, ' ')) end, {})
