ae2dfb3174a60a7281305b3649c7ce0da5bb3b3a
Vladislav Doster <mvdoster@gmail.com>
2022-09-09 00:00:14 -0500

    maint: move colorscheme from after/ to plugins/
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ plugin/onedarkpro.lua
@@ -0,0 +1,9 @@
+local ok, onedarkpro = pcall(require, 'onedarkpro')
+if not ok then
+  vim.cmd.colorscheme { 'industry' }
+  return
+else
+  onedarkpro.setup {}
+  onedarkpro.load()
+  vim.cmd.colorscheme { 'onedarkpro' }
+end
