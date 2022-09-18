72c68e4344c90258018325462a719697d3fca818
Vladislav Doster <mvdoster@gmail.com>
2022-09-18 03:18:27 -0500

    style: reformat plugin/onedarkpro.lua
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/onedarkpro.lua
+++ plugin/onedarkpro.lua
@@ -1,9 +1,9 @@
 local ok, onedarkpro = pcall(require, 'onedarkpro')
 if not ok then
-  vim.cmd.colorscheme { 'industry' }
+  vim.cmd.colorscheme {'industry'}
   return
 else
   onedarkpro.setup {}
   onedarkpro.load()
-  vim.cmd.colorscheme { 'onedarkpro' }
+  vim.cmd.colorscheme {'onedarkpro'}
 end
