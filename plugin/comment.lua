8ac4671ffda303af8ecbfc24108ca69b4e2178f5
Vladislav Doster <mvdoster@gmail.com>
2022-10-05 11:23:17 -0500

    fix: commenting now ignores empty lines
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/comment.lua
+++ plugin/comment.lua
@@ -1,6 +1,6 @@
 local ok, comment = pcall(require, 'Comment')
 if not ok then return end
-comment.setup()
+comment.setup({ignore='^$'})
 
 local file_type = require('Comment.ft')
 file_type({'bash', 'markdown', 'sh', 'yaml', 'zsh'}, '# %s') -- Metatable magic
