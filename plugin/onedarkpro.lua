080a224704aa4f4c55a27d72e457f0af0c0852e1
Vladislav Doster <mvdoster@gmail.com>
2022-10-15 20:08:11 -0500

    fix: remove broken colorscheme onedarkpro.nvim
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/onedarkpro.lua
+++
@@ -1,9 +0,0 @@
-local ok, onedarkpro = pcall(require, 'onedarkpro')
-if not ok then
-  vim.cmd.colorscheme {'industry'}
-  return
-else
-  onedarkpro.setup {}
-  onedarkpro.load()
-  vim.cmd.colorscheme {'onedarkpro'}
-end
