b5fcc4b207ed9dd9233a8d9ef8d83f5ecded6f5c
Vladislav Doster <mvdoster@gmail.com>
2022-10-15 20:12:54 -0500

    refactor: majority of neovim option values modified
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,95 +1,108 @@
--- vim: set ft=lua fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
-local opt = vim.opt
-local g = vim.g
+-- Set options {{{
+local g, indent = vim.g, 2
 
--- {{{ Set options
-vim.cmd.syntax {'on'}
+vim.notify = function(...)
+  local args = {...}
+  vim.defer_fn(function() vim.notify(unpack(args)) end, 300)
+end
 
-local options = -- Option list
-{
-  autoindent=true,
-  compatible=false,
-  cursorcolumn=false,
-  cursorline=true,
-  expandtab=true,
-  foldlevel=0,
-  foldmethod='marker',
-  hidden=true,
-  ignorecase=true,
-  incsearch=true,
-  linebreak=true,
-  list=true,
-  listchars='trail:·',
-  modeline=true,
-  mouse='a',
-  number=true,
-  relativenumber=true,
-  scrolloff=5,
-  shellcmdflag='-ic',
-  shiftround=true,
-  shiftwidth=0,
-  showcmd=true,
-  showmatch=true,
-  showmode=false,
-  signcolumn='yes',
-  smartcase=true,
-  smartindent=true,
-  softtabstop=0,
-  splitbelow=true,
-  splitright=true,
-  tabstop=4,
-  termguicolors=true,
-  textwidth=0,
-  title=true,
-  titlestring='%f',
-  undofile=true,
-  wrap=false
-}
+if vim.fn.has('nvim-0.8') == 1 then
+  --   vim.opt.spell = true -- Put new windows below current
+  vim.opt.cmdheight = 0
+  -- make all keymaps silent by default
+  local keymap_set = vim.keymap.set
+  vim.keymap.set = function(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    return keymap_set(mode, lhs, rhs, opts)
+  end
+elseif vim.fn.has('nvim-0.9') == 1 then
+  vim.opt.splitkeep = 'screen'
+end
 
-opt.clipboard:append('unnamedplus') -- Use system clipboard (REQUIRES xclip or wl-clipboard)
-opt.iskeyword:append('-') -- Include the '-' character as a character from a word
+vim.opt.autowrite = true          -- enable auto write
+vim.opt.clipboard = 'unnamedplus' -- sync with system clipboard
+vim.opt.concealcursor = "nc"      -- Hide * markup for bold and italic
+vim.opt.conceallevel = 3          -- Hide * markup for bold and italic
+vim.opt.confirm = true            -- confirm to save changes before exiting modified buffer
+vim.opt.cursorline = true         -- Enable highlighting of the current line
+vim.opt.expandtab = true          -- Use spaces instead of tabs
 
-for key, value in pairs(options) do opt[key] = value end
+vim.opt.grepformat = '%f:%l:%c:%m'
+vim.opt.grepprg = 'rg --vimgrep'
+vim.opt.guifont = 'FiraCode Nerd Font:h12'
+vim.opt.hidden = true         -- Enable modified buffers in background
+vim.opt.ignorecase = true     -- Ignore case
+vim.opt.inccommand = 'split'  -- preview incremental substitute
+vim.opt.joinspaces = false    -- No double spaces with join after a dot
+vim.opt.list = true           -- Show some invisible characters (tabs...
+vim.opt.mouse = 'a'           -- enable mouse mode
+vim.opt.number = true         -- Print line number
+vim.opt.pumblend = 10         -- Popup blend
+vim.opt.pumheight = 10        -- Maximum number of entries in a popup
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.scrolloff = 4         -- Lines of context
+vim.opt.shiftround = true     -- Round indent
+vim.opt.shiftwidth = indent   -- Size of an indent
+vim.opt.showmode = false      -- dont show mode since we have a statusline
+vim.opt.sidescrolloff = 8     -- Columns of context
+vim.opt.signcolumn = 'yes'    -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.smartcase = true      -- Don't ignore case with capitals
+vim.opt.smartindent = true    -- Insert indents automatically
+
+vim.opt.completeopt = 'menu,menuone,noselect'
+vim.opt.fillchars = {foldopen='', foldclose=''}
+vim.opt.sessionoptions = {'buffers', 'curdir', 'tabpages', 'winsize'}
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.tabstop = indent -- Number of spaces tabs count for
+vim.opt.termguicolors = true -- True color support
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- save swap file and trigger CursorHold
+vim.opt.wildmode = 'longest:full,full' -- Command-line completion mode
+vim.opt.wrap = false -- Disable line wrap
+-- vim.o.shortmess = "IToOlxfitn"
 -- }}}
 
--- {{{ Set file extensions
-vim.filetype.add({extension={tf='hcl'}, pattern={['.*%.env.*']='sh', ['ignore$']='conf'}})
+-- Set file extensions {{{
+-- vim.filetype.add({extension={tf='hcl'}, pattern={['.*%.env.*']='sh', ['ignore$']='conf'}})
 -- }}}
 
--- {{{ Disable built-in vim plugins
+--  Disable built-in vim plugins {{{
 local default_plugins = {
-  '2html_plugin',
-  'bugreport',
-  'compiler',
-  'ftplugin',
-  'getscript',
-  'getscriptPlugin',
   'gzip',
-  'logipat',
-  'matchit',
-  'netrw',
-  'netrwFileHandlers',
-  'netrwPlugin',
-  'netrwSettings',
-  'optwin',
-  'rplugin',
-  'rrhelper',
-  'spellfile_plugin',
-  'synmenu',
-  'syntax',
+  'zip',
+  'zipPlugin',
+  'fzf',
   'tar',
   'tarPlugin',
-  'tutor',
+  'getscript',
+  'getscriptPlugin',
   'vimball',
   'vimballPlugin',
-  'zip',
-  'zipPlugin'
+  '2html_plugin',
+  'matchit',
+  'matchparen',
+  'logiPat',
+  'rrhelper',
+  'netrw',
+  'netrwPlugin',
+  'netrwSettings',
+  'netrwFileHandlers'
 }
 
 for _, plugin in pairs(default_plugins) do g['loaded_' .. plugin] = 1 end
 -- }}}
 
--- {{{ Set default langiage providers
+-- Set default langiage providers {{{
 local default_providers = {'node', 'perl', 'python3', 'ruby'}
 for _, provider in ipairs(default_providers) do vim.g['loaded_' .. provider .. '_provider'] = 0 end
 -- }}}
+
+-- Use proper syntax highlighting in code blocks {{{
+local fences = {'console=sh', 'javascript', 'js=javascript', 'json', 'lua', 'python', 'sh', 'shell=sh'}
+vim.g.markdown_fenced_languages = fences
+-- }}}
+
+-- vim: set ft=lua fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
