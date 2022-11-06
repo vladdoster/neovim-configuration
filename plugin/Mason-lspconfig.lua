549e3ff1fa15ebf8db2b2082db4fa82b1a17a18c
Vladislav Doster <mvdoster@gmail.com>
2022-11-06 07:42:07 -0600

    maint: add vim to globals in sumneko_lua/selene
    - define `vim` as global variables to cut down on diagnostic noise
    - instantiate neodev plugin
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/Mason-lspconfig.lua
+++ plugin/Mason-lspconfig.lua
@@ -3,14 +3,28 @@ local ok_1, neodev = pcall(require, 'neodev')
 local ok_2, lspconfig = pcall(require, 'lspconfig')
 if not ok or not ok_1 or not ok_2 then return end
 
-require('mason-lspconfig').setup {}
-require('mason-lspconfig').setup_handlers {
+neodev.setup({})
+mason_lspconfig.setup {}
+mason_lspconfig.setup_handlers {
   function(server_name) lspconfig[server_name].setup {} end,
   ['jsonls']=function() lspconfig.jsonls.setup {settings={json={schemas=require('schemastore').json.schemas()}}} end,
-  ['sumneko_lua']=function() lspconfig.sumneko_lua.setup({settings={Lua={completion={callSnippet='Replace'}}}}) end,
+  ['sumneko_lua']=function()
+    lspconfig.sumneko_lua.setup({
+      settings={
+        Lua={
+          completion={callSnippet='Replace'},
+          runtime={version='LuaJIT'},
+          diagnostics={globals={'vim'}},
+          workspace={library=vim.api.nvim_get_runtime_file('', true)},
+          telemetry={enable=false}
+        }
+      }
+    })
+  end,
   ['yamlls']=function()
     lspconfig.yamlls.setup {
       settings={yaml={hover=true, completion=true, validate=true, schemas=require('schemastore').json.schemas()}}
     }
   end
 }
+
