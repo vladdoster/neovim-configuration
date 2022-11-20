b75b71a150e15784d0369bc138ab39a8ad9273df
Vladislav Doster <mvdoster@gmail.com>
2022-11-20 09:57:25 -0600

    style: nvim-autopairs config formatting
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/nvim-autopairs.lua
+++ plugin/nvim-autopairs.lua
@@ -2,7 +2,7 @@ local ok, autopairs = pcall(require, 'nvim-autopairs')
 if not ok then return end
 
 autopairs.setup({
-  enable_check_bracket_line = true,
-  ignored_next_char = "[%w%.]" -- will ignore alphanumeric and `.` symbol
+  enable_check_bracket_line=true,
+  ignored_next_char='[%w%.]' -- will ignore alphanumeric and `.` symbol
 
 })
