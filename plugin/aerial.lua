f864f97efd3246c960e5566f7f51c2689755bde5
Vladislav Doster <mvdoster@gmail.com>
2022-12-04 15:36:51 -0600

    fix: aerial configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/aerial.lua
+++ plugin/aerial.lua
@@ -1,6 +1,20 @@
 local ok, aerial = pcall(require, 'aerial')
 if not ok then return end
 aerial.setup {
-  backends={'treesitter', 'lsp', 'markdown'},
-  layout={width=40, default_direction='right', placement='edge'}
+  attach_mode='global',
+  backends={'lsp', 'treesitter', 'markdown', 'man'},
+  filter_kind=false,
+  guides={last_item='└ ', mid_item='├ ', nested_top='│ ', whitespace='  '},
+  keymaps={
+    ['[Y']='actions.prev_up',
+    ['[[']=false,
+    ['[y']='actions.prev',
+    [']Y']='actions.next_up',
+    [']]']=false,
+    [']y']='actions.next',
+    ['{']=false,
+    ['}']=false
+  },
+  layout={default_direction='right', min_width=28, placement='edge'},
+  show_guides=true
 }
