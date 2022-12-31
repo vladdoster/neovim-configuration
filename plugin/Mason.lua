6546f6a0caea851df2564889845c5c50df68c276
Vladislav Doster <mvdoster@gmail.com>
2022-12-31 01:24:34 -0600

    fix: mason.nvim log level & icons
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/Mason.lua
+++ plugin/Mason.lua
@@ -1,9 +1,9 @@
 local ok, mason = pcall(require, 'mason')
 if not ok then return end
 mason.setup {
-  log_level=vim.log.levels.ERROR,
+  log_level=vim.log.levels.WARN,
   max_concurrent_installers=10,
-  ui={icons={package_installed='', package_pending='', package_uninstalled=''}}
+  ui={icons={package_installed='•', package_pending='•', package_uninstalled='•'}}
 }
 
 local lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
