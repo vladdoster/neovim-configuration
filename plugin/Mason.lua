326e84c6361c59a1495db5f17dba601752fffb48
Vladislav Doster <mvdoster@gmail.com>
2023-02-17 10:11:46 -0600

    fix: typo in mason.lua configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/Mason.lua
+++ plugin/Mason.lua
@@ -1,9 +1,10 @@
 local ok, mason = pcall(require, 'mason')
 if not ok then return end
+
 mason.setup {
   log_level=vim.log.levels.WARN,
   max_concurrent_installers=10,
   ui={icons={package_installed='•', package_pending='•', package_uninstalled='•'}}
 }
 
-ocal lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
+local lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
