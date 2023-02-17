545872e0bca13f18da17e8a96580057989843dc0
Vladislav Doster <mvdoster@gmail.com>
2023-02-17 08:53:08 -0600

    fix: remove fragile mason cmd
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- plugin/Mason.lua
+++ plugin/Mason.lua
@@ -6,6 +6,4 @@ mason.setup {
   ui={icons={package_installed='•', package_pending='•', package_uninstalled='•'}}
 }
 
-local lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
-local usr_cmd = vim.api.nvim_create_user_command
-usr_cmd('MasonInstallAll', function() vim.cmd('MasonInstall ' .. table.concat(lsp, ' ')) end, {})
+ocal lsp = {'bash-language-server', 'hadolint', 'lua-language-server', 'luaformatter', 'pyright', 'selene'}
