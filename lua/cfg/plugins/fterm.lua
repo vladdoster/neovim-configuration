050cd8efeaddd394c43509c8005a6263b9040da1
Vladislav Doster <mvdoster@gmail.com>
2023-03-20 03:51:22 -0500

    fix: fterm keybinds
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/plugins/fterm.lua
+++ lua/cfg/plugins/fterm.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<A-i>', '<CMD>lua require(\'FTerm\').toggle()<CR>')
-vim.keymap.set('t', '<A-i>', '<C-\\><C-n><CMD>lua require(\'FTerm\').toggle()<CR>')
-vim.keymap.set('n', '<A-g>',
-               function() require('FTerm'):new({cmd={'gitui'}, dimensions={height=0.9, width=0.9}}):open() end)
+local keymap = vim.keymap.set
+
+keymap('n', '<A-t>', '<CMD>lua require(\'FTerm\').toggle()<CR>')
+keymap('t', '<A-t>', '<C-\\><C-n><CMD>lua require(\'FTerm\').toggle()<CR>')
