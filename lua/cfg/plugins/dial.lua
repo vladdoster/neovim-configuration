e0d1c404884995c1ca60be67a236f21b4d3cea57
Vladislav Doster <mvdoster@gmail.com>
2023-03-20 03:52:23 -0500

    feat: add dial.nvim configration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

---
+++ lua/cfg/plugins/dial.lua
@@ -0,0 +1,18 @@
+local augend = require('dial.augend')
+require('dial.config').augends:register_group{
+  -- default augends used when no group name is specified
+  default={
+    augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+    augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+    augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
+    augend.constant.alias.bool -- boolean value (true <-> false)
+  }
+}
+
+local map = vim.keymap.set
+map('n', '<C-a>', require('dial.map').inc_normal(), {noremap=true})
+map('n', '<C-x>', require('dial.map').dec_normal(), {noremap=true})
+map('v', '<C-a>', require('dial.map').inc_visual(), {noremap=true})
+map('v', '<C-x>', require('dial.map').dec_visual(), {noremap=true})
+map('v', 'g<C-a>', require('dial.map').inc_gvisual(), {noremap=true})
+map('v', 'g<C-x>', require('dial.map').dec_gvisual(), {noremap=true})
