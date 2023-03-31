81375d41069a9ecd2d5fff4a748dc2491c500b2e
Vladislav Doster <mvdoster@gmail.com>
2023-03-31 04:25:09 -0500

    feat: update autocmds
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/autocmd.lua
+++ lua/cfg/autocmd.lua
@@ -3,30 +3,85 @@ local A = vim.api
 vim.filetype.add({
   extension={conf='conf', mdx='markdown', mjml='html'},
   pattern={['.*%.env.*']='sh', ['ignore$']='conf'},
-  filename={['yup.lock']='yaml'}
 })
-local num_au = A.nvim_create_augroup('CFG', {clear=true})
--- Highlight the region on yank
-A.nvim_create_autocmd('TextYankPost', {group=num_au, callback=function() vim.highlight.on_yank({higroup='Visual'}) end})
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- Remove useless stuff from the terminal window and enter INSERT mode
-A.nvim_create_autocmd('TermOpen', {
-  group=num_au,
-  callback=function(data)
-    if not string.find(vim.bo[data.buf].filetype, '^[fF][tT]erm') then
-      A.nvim_set_option_value('number', false, {scope='local'})
-      A.nvim_set_option_value('relativenumber', false, {scope='local'})
-      A.nvim_set_option_value('signcolumn', 'no', {scope='local'})
-      A.nvim_command('startinsert')
-    end
-  end
+vim.api.nvim_create_autocmd('TermOpen', {
+	group=augroup('term_open'),
+	callback=function(data)
+		if not string.find(vim.bo[data.buf].filetype, '^[fF][tT]erm') then
+			A.nvim_set_option_value('number', false, {scope='local'})
+			A.nvim_set_option_value('relativenumber', false, {scope='local'})
+			A.nvim_set_option_value('signcolumn', 'no', {scope='local'})
+			A.nvim_command('startinsert')
+		end
+	end
 })
--- Jump to the last place in the file before exiting
-A.nvim_create_autocmd('BufReadPost', {
-  group=num_au,
-  callback=function(data)
-    local last_pos = A.nvim_buf_get_mark(data.buf, '"')
-    if last_pos[1] > 0 and last_pos[1] <= A.nvim_buf_line_count(data.buf) then A.nvim_win_set_cursor(0, last_pos) end
-  end
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  command = "checktime",
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "startuptime",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
 })
 
 -- vim: set fenc=utf8 ffs=unix ft=lua list noet sw=2 ts=2 tw=72:
