32273b5ab0fe1403ccd25919833773eb96a04ac9
Vladislav Doster <mvdoster@gmail.com>
2023-04-01 03:53:39 -0500

    fix: vim-easy-align keymaps and configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/plugins/easyalign.lua
+++
@@ -1,4 +0,0 @@
--- EasyAlign
-local function map(m, k, v) vim.keymap.set(m, k, v, {silent=true}) end
-
-map('v', '<C-a>', [[<CMD>EasyAlign*\]])
---
+++ lua/cfg/plugins/vim-easy-align.lua
@@ -0,0 +1,5 @@
+-- start interactive EasyAlign in visual mode (e.g. vipga)
+vim.api.nvim_set_keymap('x', 'ga', '<Plug>(EasyAlign)', {})
+-- Start interactive EasyAlign for a motion/text object (e.g. gaip)
+vim.api.nvim_set_keymap('n', 'ga', '<Plug>(EasyAlign)', {})
+vim.api.nvim_set_keymap('v', '<leader><bslash>', ':EasyAlign*<bslash><CR>', {})
