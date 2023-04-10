b0e52f0bd838f7581d63a6fd9d62cb6b13a77d5f
Vladislav Doster <mvdoster@gmail.com>
2023-04-10 05:57:12 -0500

    fix: changes for v2.x & install all servers
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/plugins/lsp/null-ls.lua
+++ lua/cfg/plugins/lsp/null-ls.lua
@@ -28,11 +28,15 @@ nls.setup({
     U.mappings(bufnr)
   end
 })
-
+-- require("mason-null-ls").setup({
+-- 	automatic_installation = true,
+-- })
+-- ensure_installed={'bash-language-server', 'editorconfig-checker', 'lua-language-server'},
 require('mason-null-ls').setup({
-  ensure_installed={'bash-language-server', 'editorconfig-checker', 'lua-language-server'},
+  -- TODO: narrow down list if performance suffers
+  ensure_installed=require('mason-lspconfig').get_available_servers(),
+  max_concurrent_installers=50,
   automatic_installation=true,
-  automatic_setup=false
+  handlers={function() end}
 })
-
 -- vim: set fenc=utf8 ffs=unix ft=lua list noet sw=2 ts=2 tw=72:
