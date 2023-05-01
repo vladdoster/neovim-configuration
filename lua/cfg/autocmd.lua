df0fc14245945c082729cbd413cf38c4f9b68c69
Vladislav Doster <mvdoster@gmail.com>
2023-05-01 17:47:22 -0500

    fix: remove unused autocmd
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/autocmd.lua
+++ lua/cfg/autocmd.lua
@@ -22,18 +22,6 @@ vim.api.nvim_create_autocmd({'VimResized'}, {
   end
 })
 
--- -- go to last loc when opening a buffer
--- vim.api.nvim_create_autocmd("BufReadPost", {
---   group = augroup("last_loc"),
---   callback = function()
---     local mark = vim.api.nvim_buf_get_mark(0, '"')
---     local lcount = vim.api.nvim_buf_line_count(0)
---     if mark[1] > 0 and mark[1] <= lcount then
---       pcall(vim.api.nvim_win_set_cursor, 0, mark)
---     end
---   end,
--- })
-
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd('FileType', {
   group=augroup('close_with_q'),
