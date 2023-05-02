69f74677390fa5ba06bf15a8419fc571c8f0fc0c
Vladislav Doster <mvdoster@gmail.com>
2023-05-02 07:06:13 -0500

    fix: telescope file and git file keymaps
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/plugins/telescope.lua
+++ lua/cfg/plugins/telescope.lua
@@ -30,18 +30,9 @@ require('telescope').setup({
 })
 local map = vim.api.nvim_set_keymap
 
-local function files_fallback()
-  vim.fn.system'git rev-parse --is-inside-work-tree'
-  local dropdown = require('telescope.themes').get_ivy()
-  if vim.v.shell_error == 0 then
-    require('telescope.builtin').git_files(dropdown)
-  else
-    require('telescope.builtin').find_files(dropdown)
-  end
-end
-
-local silent = {silent=true, noremap=true}
+local btm = require('telescope.themes').get_ivy()
+local silent, tbl = {silent=true, noremap=true}, table.unpack
 map('n', '\'b', [[<cmd>Telescope buffers show_all_buffers=true<cr>]], silent)
-map('n', '<c-d>', '', {silent=true, noremap=true, callback=files_fallback})
+map('n', '\'g', '', {tbl(silent), callback=function() finders.git_files(btm) end})
+map('n', '\'f', '', {tbl(silent), callback=function() finders.find_files(btm) end})
 map('n', '\'r', [[<cmd>Telescope live_grep<cr>]], silent)
-map('n', '<c-p>', [[<cmd>Telescope commands <cr>]], silent)
