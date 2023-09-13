6a6fb42783eef17e96360d69b693cb8fd05cd377
Vladislav Doster <mvdoster@gmail.com>
2023-09-13 03:44:51 -0500

    feat: set smoothscrolling option
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/settings.lua
+++ lua/cfg/settings.lua
@@ -34,6 +34,7 @@ opt.sidescrolloff = 8 -- Columns of context
 opt.signcolumn = 'yes' -- Always show the signcolumn, otherwise it would shift the text each time
 opt.smartcase = true -- Don't ignore case with capitals
 opt.smartindent = true -- Insert indents automatically
+opt.smoothscroll = true
 opt.spelllang = {'en'}
 opt.splitbelow = true -- Put new windows below current
 opt.splitright = true -- Put new windows right of current
