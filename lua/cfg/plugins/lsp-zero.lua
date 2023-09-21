1210207ca1c97a3b7ca4d7a877efd1936f6e2500
Vladislav Doster <mvdoster@gmail.com>
2023-09-21 18:11:57 -0500

    feat: add lsp-zero plugin configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/cfg/plugins/lsp-zero.lua
+++ lua/cfg/plugins/lsp-zero.lua
@@ -1,42 +1,14 @@
 local lsp = require('lsp-zero').preset({})
-local lsp_zero = require('lsp-zero')
--- You need to setup `cmp` after lsp-zero
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-vim.lsp.set_log_level('debug')
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer=bufnr})
+end)
 
-cmp.setup({
-  sources={
-    {name='nvim_lsp'},
-    {name='treesitter'},
-    {name='buffer'},
-    {name='luasnip'},
-    {name='path'},
-  },
-  window={completion={winhighlight='Normal:Pmenu,FloatBorder:Pmenu,Search:None', col_offset=-3, side_padding=0}},
-  mapping={
-    ['<CR>']=cmp.mapping.confirm({select=false}),
-    ['<C-Space>']=cmp.mapping.complete(),
-    ['<C-f>']=cmp_action.luasnip_jump_forward(),
-    ['<C-b>']=cmp_action.luasnip_jump_backward()
-  },
-  snippet={
-    expand=function(args)
-      require('luasnip').lsp_expand(args.body)
-    end
-  }
-})
+lsp.ensure_installed({'pylsp', 'lua-language-server'})
 
+-- require'lspconfig'.pyright.setup{}
+require'lspconfig'.pylsp.setup{}
+-- require('lspconfig').pyright.setup({})
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-local servers = {'eslint', 'gopls','bashls', 'html', 'jsonls', 'pyright', 'terraformls', 'yamlls'}
-for _, server in ipairs(servers) do
-  require('lspconfig')[server].setup({
-    capabilities=lsp_capabilities,
-    on_attach=function(client, bufnr)
-      lsp_zero.default_keymaps({buffer=bufnr})
-    end
-  })
-end
+lsp.setup()
