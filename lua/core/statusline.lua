8a39c375c4cb6fd6be88dc349747ee5a0420fc90
Vladislav Doster <mvdoster@gmail.com>
2023-10-10 15:40:01 -0500

    fix: use previous lualine configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -1,18 +1,9 @@
-----------------------------------------------------------
--- Statusline configuration file
------------------------------------------------------------
--- Plugin: feline.nvim (freddiehaddad fork)
--- URL: https://github.com/freddiehaddad/feline.nvim
--- For the configuration see the Usage section:
--- https://github.com/freddiehaddad/feline.nvim/blob/master/USAGE.md
--- Thanks to ibhagwan for the example to follow:
--- https://github.com/ibhagwan/nvim-lua
 local status_ok, lualine = pcall(require, 'lualine')
 if not status_ok then
   return
 end
 
-require('lualine').setup{
+lualine.setup{
   options={
     always_divide_middle=true,
     component_separators='',
