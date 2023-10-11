0dc6584d86e6ec96a16e9ef3c4d2ddde10bff9f9
Vladislav Doster <mvdoster@gmail.com>
2023-10-11 00:32:30 -0500

    feat: add telescope-project extension
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ if not telescope_status_ok then
   return
 end
 local actions = require('telescope.actions')
+local project_actions = require("telescope._extensions.project.actions")
 telescope.setup({
   defaults={
     initial_mode='insert',
@@ -13,12 +14,13 @@ telescope.setup({
         ["<C-p>"]=actions.cycle_history_prev,
         ["<C-j>"]=actions.move_selection_next,
         ["<C-k>"]=actions.move_selection_previous
+        -- ['<A-h>']=actions.toggle_preview,
       },
       n={q=actions.close}
     },
     -- mappings={
     --   i={
-    --     ['<A-h>']=lactions.toggle_preview,
+
     --     ['<C-n>']=actions.move_selection_next,
     --     ['<C-p>']=actions.move_selection_previous,
     --     ['<ESC>']=actions.close,
@@ -29,12 +31,32 @@ telescope.setup({
     prompt_prefix=' ᐳ ',
     sorting_strategy='ascending'
   },
-  extensions={fzf={case_mode='smart_case', fuzzy=true, override_file_sorter=true, override_generic_sorter=true}},
+  extensions={
+    project={
+      base_dirs={
+        {path='~/.config/nvim/', max_depth=1},
+        {path='~/.config/zsh', max_depth=1},
+        {path='~/.local/share/zinit', max_depth=1},
+        {path='~/code', max_depth=2}
+      },
+      hidden_files=true,
+      theme="ivy",
+      order_by="asc",
+      search_by="title",
+      on_project_selected=function(prompt_bufnr)
+        -- Do anything you want in here. For example:
+        project_actions.change_working_directory(prompt_bufnr, false)
+      end
+    },
+    fzf={case_mode='smart_case', fuzzy=true, override_file_sorter=true, override_generic_sorter=true}
+  },
   pickers={find_files={theme='ivy'}, git_files={theme='ivy'}, live_grep={theme='ivy'}}
 })
 
 local map = vim.api.nvim_set_keymap
 local silent = {silent=true, noremap=true}
 -- open with the path of the current buffer
-map("n", "<space>gb", ":Telescope git_files path=%:p:h select_buffer=true <CR>", silent)
-map('n', '\'r', [[<cmd>Telescope live_grep<cr>]], silent)
+map("n", "<space>gb", ":Telescope git_files path=%:p:h select_buffer=true <CR>", {noremap=true, silent=true})
+map('n', '\'r', ":Telescope live_grep<CR>", {noremap=true, silent=true})
+map('n', '<C-p>', ":lua require'telescope'.extensions.project.project{display_type = 'full'}<CR>",
+    {noremap=true, silent=true})
