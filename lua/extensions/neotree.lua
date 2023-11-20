1f060a2322b301b69a8e6d6dfb16a657b8a2d0e7
Vladislav Doster <mvdoster@gmail.com>
2023-11-20 17:49:10 -0600

    fix: neo-tree configuration
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/extensions/neotree.lua
+++ lua/extensions/neotree.lua
@@ -1,6 +1,4 @@
-local neotree = require("neo-tree")
-
-neotree.setup({
+require('neo-tree').setup({
   close_if_last_window=true,
   name={trailing_slash=true, use_git_status_colors=true, highlight="NeoTreeFileName"},
   window={width=25, mappings={["l"]="open", ["h"]="close_node"}},
