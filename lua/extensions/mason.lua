90894d466681e10f4f903f551694d3d7fb5a1465
Vladislav Doster <mvdoster@gmail.com>
2024-01-20 02:23:01 -0600

    fix(mason): remove unused keymap descriptions
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/extensions/mason.lua
+++ lua/extensions/mason.lua
@@ -1,74 +1,44 @@
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
-  -- NOTE: Remember that lua is a real programming language, and as such it is possible
-  -- to define small helper and utility functions so you don't have to repeat yourself
-  -- many times.
-  --
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
   local nmap = function(keys, func, desc)
     if desc then desc = 'LSP: ' .. desc end
 
     vim.keymap.set('n', keys, func, {buffer = bufnr, desc = desc})
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>rn', vim.lsp.buf.rename)
+  nmap('<leader>ca', vim.lsp.buf.code_action)
 
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
-       '[W]orkspace [S]ymbols')
-
-  -- See `:help K` for why this keymap
-  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-  nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
+  nmap('gd', require('telescope.builtin').lsp_definitions)
+  nmap('gr', require('telescope.builtin').lsp_references)
+  nmap('gI', require('telescope.builtin').lsp_implementations)
+  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions)
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols)
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+  nmap('<S-K>', vim.lsp.buf.hover)
+  nmap('<C-k>', vim.lsp.buf.signature_help)
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
-       '[W]orkspace [L]ist Folders')
-
+  nmap('gD', vim.lsp.buf.declaration)
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder)
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder)
+  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end)
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_) vim.lsp.buf.format() end,
-                                       {desc = 'Format current buffer with LSP'})
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_) vim.lsp.buf.format() end, {})
 end
-
--- document existing key chains
-
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-
-  pyright = {},
   lua_ls = {
     Lua = {
       diagnostics = {globals = {"hs", "vim"}},
       workspace = {checkThirdParty = false},
       telemetry = {enable = false}
     }
-  }
+  },
+  pyright = {}
 }
 
 -- Setup neovim lua configuration
