6fab0fb169c7fab9bea5c2ecf53dfa814f3b1085
vladislav doster <mvdoster@gmail.com>
2026-02-15 05:03:29 -0600

    fix: blink-cmp configuration for completion options
    Signed-off-by: vladislav doster <mvdoster@gmail.com>

--- lua/config/plugins/blink-cmp.lua
+++ lua/config/plugins/blink-cmp.lua
@@ -1,5 +1,14 @@
+local has_words_before = function()
+  local col = vim.api.nvim_win_get_cursor(0)[2]
+  if col == 0 then
+    return false
+  end
+  local line = vim.api.nvim_get_current_line()
+  return line:sub(col, col):match("%s") == nil
+end
+
 return {
-  { -- Autocompletion
+  {
     'saghen/blink.cmp',
     event = 'VimEnter',
     version = '1.*',
@@ -32,68 +41,53 @@ return {
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
-    opts = {
+    opts = {     
       keymap = {
-        -- 'default' (recommended) for mappings similar to built-in completions
-        --   <c-y> to accept ([y]es) the completion.
-        --    This will auto-import if your LSP supports it.
-        --    This will expand snippets if the LSP sent a snippet.
-        -- 'super-tab' for tab to accept
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- For an understanding of why the 'default' preset is recommended,
-        -- you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
-        -- <c-space>: Open menu or open docs if already open
-        -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-        -- <c-e>: Hide menu
-        -- <c-k>: Toggle signature help
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        preset = 'none',
+        -- If completion hasn't been triggered yet, insert the first suggestion; if it has, cycle to the next suggestion.
+        ['<Tab>'] = {
+          function(cmp)
+            if has_words_before() then
+              return cmp.insert_next()
+            end
+          end,
+          'fallback',
+        },
+        -- Navigate to the previous suggestion or cancel completion if currently on the first one.
+        ['<S-Tab>'] = { 'insert_prev' },
       },
-
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
         -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
       },
-
       completion = {
+        menu = { enabled = false },
+        list = { selection = { preselect = false }, cycle = { from_top = false } },
         -- By default, you may press `<c-space>` to show the documentation.
-        -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = false, auto_show_delay_ms = 500 },
       },
-
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'buffer', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
       },
-
       snippets = { preset = 'luasnip' },
-
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
       -- By default, we use the Lua implementation instead, but you may enable
       -- the rust implementation via `'prefer_rust_with_warning'`
-      --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
-
+      fuzzy = {
+        implementation = 'lua',
+        sorts = {
+          'exact',
+          'score',
+          'sort_text',
+        },
+      }
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
     },
   },
 }
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=2 sts=2 sw=2 noet
