7a54a91744856a3625ed6dc37de56f21f1e133f0
Vladislav Doster <mvdoster@gmail.com>
2026-09-24 20:30:51 -0500

    refactor(keys): stop shadowing built-in maps
    mini.surround, mini.ai and sort.nvim took s, an/in, go, is/as and
    ]s/[s, and now use gs*, aN/iN, gS, iS/aS and ],/[,. Visual maps use
    x mode, gitsigns drops the duplicate <leader>hu, and DAP breakpoints
    move to <leader>db and <leader>dB. mini.nvim uses its new nvim-mini
    repository.
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- lua/config/plugins/debug.lua
+++ lua/config/plugins/debug.lua
@@ -48,12 +48,12 @@ return {
       desc = 'Debug: Step Out',
     },
     {
-      '<leader>b',
+      '<leader>db',
       function() require('dap').toggle_breakpoint() end,
       desc = 'Debug: Toggle Breakpoint',
     },
     {
-      '<leader>B',
+      '<leader>dB',
       function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end,
       desc = 'Debug: Set Breakpoint',
     },
